2ac0b6c39751400ab2495f315ab52ece6ecadaf3
SnappyDragon64 <pg61151@gmail.com>
2021-01-06 16:34:33 +0530

    Update BGTileEntityTypes.java

--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGTileEntityTypes.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGTileEntityTypes.java
@@ -7,7 +7,7 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
-public class BGTileEntities {
+public class BGTileEntityTypes {
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, BloomAndGloom.MOD_ID);
 
     public static final RegistryObject<TileEntityType<RafflesiaTileEntity>> RAFFLESIA = TILE_ENTITY_TYPES.register("rafflesia",
