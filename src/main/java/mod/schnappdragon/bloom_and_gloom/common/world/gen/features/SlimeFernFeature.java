3c6c4ce285b07a6f46ca2b0449183015cbe3084e
SnappyDragon64 <pg61151@gmail.com>
2021-01-08 17:15:03 +0530

    World Gen Fixes

--- src/main/java/mod/schnappdragon/bloom_and_gloom/common/world/gen/features/SlimeFernFeature.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/common/world/gen/features/SlimeFernFeature.java
@@ -28,38 +28,50 @@ public class SlimeFernFeature extends Feature<BlockClusterFeatureConfig> {
     public boolean generate(ISeedReader reader, ChunkGenerator generator, Random rand, BlockPos pos, BlockClusterFeatureConfig config) {
         ChunkPos chunkPos = new ChunkPos(pos);
         if (SharedSeedRandom.seedSlimeChunk(chunkPos.x, chunkPos.z, reader.getSeed(), 987234911L).nextInt(10) == 0) {
-
             int i = 0;
             BlockPos.Mutable blockpos$mutable = new BlockPos.Mutable();
+            Direction[] directions = new Direction[]{Direction.DOWN, Direction.NORTH, Direction.EAST, Direction.SOUTH, Direction.WEST, Direction.UP};
 
             for (int j = 0; j < config.tryCount; ++j) {
                 blockpos$mutable.setAndOffset(pos, rand.nextInt(config.xSpread + 1) - rand.nextInt(config.xSpread + 1), rand.nextInt(config.ySpread + 1) - rand.nextInt(config.ySpread + 1), rand.nextInt(config.zSpread + 1) - rand.nextInt(config.zSpread + 1));
+
                 if ((reader.isAirBlock(blockpos$mutable) || config.isReplaceable && reader.getBlockState(blockpos$mutable).getMaterial().isReplaceable())) {
-                    boolean flag = false;
+                    for (Direction dir : directions) {
+                        if (reader.getBlockState(blockpos$mutable.offset(dir)).isSolidSide(reader, blockpos$mutable, dir.getOpposite())) {
+                            BlockState state;
+                            if (dir == Direction.DOWN)
+                                state = BGBlocks.SLIME_FERN.get().getDefaultState();
+                            else if (dir == Direction.UP)
+                                state = BGBlocks.SLIME_FERN.get().getDefaultState().with(SlimeFernBlock.ON_CEILING, true);
+                            else
+                                state = BGBlocks.WALL_SLIME_FERN.get().getDefaultState().with(WallSlimeFernBlock.HORIZONTAL_FACING, dir.getOpposite());
 
-                    for(Direction direction : Direction.Plane.HORIZONTAL) {
-                        if (!flag) {
-                            BlockState anchorState = reader.getBlockState(pos.offset(direction));
-                            if (anchorState.isSolidSide(reader, blockpos$mutable, direction.getOpposite())) {
-                                config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.WALL_SLIME_FERN.get().getDefaultState().with(WallSlimeFernBlock.HORIZONTAL_FACING, direction), rand);
-                                ++i;
-                                flag = true;
-                            }
+                            config.blockPlacer.place(reader, blockpos$mutable, state, rand);
+                            ++i;
+                            break;
                         }
                     }
-
-                    if (!flag) {
-                        BlockState upState = reader.getBlockState(pos.up());
-                        BlockState downState = reader.getBlockState(pos.down());
-                        if (upState.isSolidSide(reader, blockpos$mutable, Direction.DOWN)) {
-                            config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.SLIME_FERN.get().getDefaultState(), rand);
-                            ++i;
-                        }
-                        else if (downState.isSolidSide(reader, blockpos$mutable, Direction.UP)) {
-                            config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.SLIME_FERN.get().getDefaultState().with(SlimeFernBlock.ON_CEILING, true), rand);
-                            ++i;
-                        }
+                    /*
+                    if (reader.getBlockState(blockpos$mutable.down()).isSolidSide(reader, blockpos$mutable, Direction.UP)) {
+                        config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.SLIME_FERN.get().getDefaultState(), rand);
+                        ++i;
+                    } else if (reader.getBlockState(blockpos$mutable.north()).isSolidSide(reader, blockpos$mutable, Direction.SOUTH)) {
+                        config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.WALL_SLIME_FERN.get().getDefaultState().with(WallSlimeFernBlock.HORIZONTAL_FACING, Direction.SOUTH), rand);
+                        ++i;
+                    } else if (reader.getBlockState(blockpos$mutable.east()).isSolidSide(reader, blockpos$mutable, Direction.WEST)) {
+                        config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.WALL_SLIME_FERN.get().getDefaultState().with(WallSlimeFernBlock.HORIZONTAL_FACING, Direction.WEST), rand);
+                        ++i;
+                    } else if (reader.getBlockState(blockpos$mutable.south()).isSolidSide(reader, blockpos$mutable, Direction.NORTH)) {
+                        config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.WALL_SLIME_FERN.get().getDefaultState().with(WallSlimeFernBlock.HORIZONTAL_FACING, Direction.NORTH), rand);
+                        ++i;
+                    } else if (reader.getBlockState(blockpos$mutable.west()).isSolidSide(reader, blockpos$mutable, Direction.EAST)) {
+                        config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.WALL_SLIME_FERN.get().getDefaultState().with(WallSlimeFernBlock.HORIZONTAL_FACING, Direction.EAST), rand);
+                        ++i;
+                    } else if (reader.getBlockState(blockpos$mutable.up()).isSolidSide(reader, blockpos$mutable, Direction.DOWN)) {
+                        config.blockPlacer.place(reader, blockpos$mutable, BGBlocks.SLIME_FERN.get().getDefaultState().with(SlimeFernBlock.ON_CEILING, true), rand);
+                        ++i;
                     }
+                     */
                 }
             }
             return i > 0;
