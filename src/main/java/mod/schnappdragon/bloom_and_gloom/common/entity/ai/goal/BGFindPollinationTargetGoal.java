96e79bb1f17c9fa53869c3e9178ae7058947875c
SnappyDragon64 <pg61151@gmail.com>
2021-01-14 18:39:16 +0530

    Bees can pollinate kabloom bushes

---
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/common/entity/ai/goal/BGFindPollinationTargetGoal.java
@@ -0,0 +1,63 @@
+package mod.schnappdragon.bloom_and_gloom.common.entity.ai.goal;
+
+import mod.schnappdragon.bloom_and_gloom.common.block.KabloomBushBlock;
+import mod.schnappdragon.bloom_and_gloom.core.registry.BGBlocks;
+import net.minecraft.block.*;
+import net.minecraft.entity.ai.goal.Goal;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraft.state.IntegerProperty;
+import net.minecraft.tags.BlockTags;
+import net.minecraft.util.math.BlockPos;
+
+public class BGFindPollinationTargetGoal extends Goal {
+    private final BeeEntity bee;
+
+    public BGFindPollinationTargetGoal(BeeEntity bee) {
+        this.bee = bee;
+    }
+
+    public boolean shouldExecute() {
+        return this.canBeeExecute() && !this.bee.func_233678_J__();
+    }
+
+    public boolean shouldContinueExecuting() {
+        return this.canBeeExecute() && !this.bee.func_233678_J__();
+    }
+
+    public boolean canBeeExecute() {
+        if (this.bee.getCropsGrownSincePollination() >= 10) {
+            return false;
+        } else if (this.bee.getRNG().nextFloat() < 0.3F) {
+            return false;
+        } else {
+            return this.bee.hasNectar() && this.bee.isHiveValid();
+        }
+    }
+
+    public void tick() {
+        if (this.bee.getRNG().nextInt(30) == 0) {
+            for(int i = 1; i <= 2; ++i) {
+                BlockPos blockpos = this.bee.getPosition().down(i);
+                BlockState blockstate = this.bee.world.getBlockState(blockpos);
+                Block block = blockstate.getBlock();
+                boolean flag = false;
+                IntegerProperty integerproperty = null;
+                if (block.isIn(BlockTags.BEE_GROWABLES)) {
+                    if (block == BGBlocks.KABLOOM_BUSH.get()) {
+                        int k = blockstate.get(KabloomBushBlock.AGE);
+                        if (k < 7) {
+                            flag = true;
+                            integerproperty = KabloomBushBlock.AGE;
+                        }
+                    }
+
+                    if (flag) {
+                        this.bee.world.playEvent(2005, blockpos, 0);
+                        this.bee.world.setBlockState(blockpos, blockstate.with(integerproperty, blockstate.get(integerproperty) + 1));
+                        this.bee.addCropCounter();
+                    }
+                }
+            }
+        }
+    }
+}
--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/event/BGEvents.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/event/BGEvents.java
@@ -1,8 +1,23 @@
 package mod.schnappdragon.bloom_and_gloom.core.event;
 
+import mod.schnappdragon.bloom_and_gloom.common.entity.ai.goal.BGFindPollinationTargetGoal;
 import mod.schnappdragon.bloom_and_gloom.core.BloomAndGloom;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.BeeEntity;
+import net.minecraftforge.event.entity.EntityJoinWorldEvent;
+import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = BloomAndGloom.MOD_ID)
 public class BGEvents {
+
+    @SubscribeEvent
+    public static void addGoalToBee(EntityJoinWorldEvent event) {
+        Entity entity = event.getEntity();
+        if (entity.getType() == EntityType.BEE) {
+            BeeEntity bee = (BeeEntity) entity;
+            bee.goalSelector.addGoal(7, new BGFindPollinationTargetGoal(bee));
+        }
+    }
 }
