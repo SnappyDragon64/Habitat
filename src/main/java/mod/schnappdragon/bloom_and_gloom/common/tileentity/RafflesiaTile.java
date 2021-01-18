7c1bc2c13e63685e884ac5500cd748ce6c20b0f1
SnappyDragon64 <pg61151@gmail.com>
2021-01-18 16:25:41 +0530

    Internal class renaming

--- src/main/java/mod/schnappdragon/bloom_and_gloom/common/tileentity/RafflesiaTile.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/common/tileentity/RafflesiaTile.java
@@ -11,10 +11,10 @@ import net.minecraft.world.World;
 
 import javax.annotation.*;
 
-public class RafflesiaTileEntity extends TileEntity {
+public class RafflesiaTile extends TileEntity {
     public ListNBT Effects = new ListNBT();
 
-    public RafflesiaTileEntity() {
+    public RafflesiaTile() {
         super(BGTileEntityTypes.RAFFLESIA.get());
         CompoundNBT tag = new CompoundNBT();
         tag.putByte("EffectId", (byte) 19);
--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGTileEntityTypes.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGTileEntityTypes.java
@@ -1,7 +1,7 @@
 package mod.schnappdragon.bloom_and_gloom.core.registry;
 
 import mod.schnappdragon.bloom_and_gloom.core.BloomAndGloom;
-import mod.schnappdragon.bloom_and_gloom.common.tileentity.RafflesiaTileEntity;
+import mod.schnappdragon.bloom_and_gloom.common.tileentity.RafflesiaTile;
 import net.minecraft.tileentity.TileEntityType;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
@@ -10,6 +10,6 @@ import net.minecraftforge.registries.ForgeRegistries;
 public class BGTileEntityTypes {
     public static final DeferredRegister<TileEntityType<?>> TILE_ENTITY_TYPES = DeferredRegister.create(ForgeRegistries.TILE_ENTITIES, BloomAndGloom.MOD_ID);
 
-    public static final RegistryObject<TileEntityType<RafflesiaTileEntity>> RAFFLESIA = TILE_ENTITY_TYPES.register("rafflesia",
-            () -> TileEntityType.Builder.create(RafflesiaTileEntity::new, BGBlocks.RAFFLESIA.get()).build(null));
+    public static final RegistryObject<TileEntityType<RafflesiaTile>> RAFFLESIA = TILE_ENTITY_TYPES.register("rafflesia",
+            () -> TileEntityType.Builder.create(RafflesiaTile::new, BGBlocks.RAFFLESIA.get()).build(null));
 }
