43e6b036096005e5fd3618453075875223854847
SnappyDragon64 <pg61151@gmail.com>
2021-01-21 01:31:59 +0530

    Kabloom Explosions have been redone.
    Finally.

--- src/main/java/mod/schnappdragon/bloom_and_gloom/common/entity/projectile/KabloomFruitEntity.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/common/entity/projectile/KabloomFruitEntity.java
@@ -4,6 +4,8 @@ import mod.schnappdragon.bloom_and_gloom.core.misc.BGDamageSources;
 import mod.schnappdragon.bloom_and_gloom.core.registry.BGEntityTypes;
 import mod.schnappdragon.bloom_and_gloom.core.registry.BGItems;
 import mod.schnappdragon.bloom_and_gloom.core.registry.BGSoundEvents;
+import net.minecraft.enchantment.ProtectionEnchantment;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.LivingEntity;
 import net.minecraft.entity.item.ItemEntity;
@@ -13,16 +15,17 @@ import net.minecraft.item.ItemStack;
 import net.minecraft.network.IPacket;
 import net.minecraft.particles.ItemParticleData;
 import net.minecraft.particles.ParticleTypes;
+import net.minecraft.particles.RedstoneParticleData;
 import net.minecraft.util.SoundCategory;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.RayTraceContext;
 import net.minecraft.util.math.RayTraceResult;
 import net.minecraft.util.math.vector.Vector3d;
-import net.minecraft.world.Explosion;
+import net.minecraft.world.GameRules;
 import net.minecraft.world.World;
 import net.minecraftforge.api.distmarker.Dist;
 import net.minecraftforge.api.distmarker.OnlyIn;
 import net.minecraftforge.fml.network.NetworkHooks;
-
-import javax.annotation.ParametersAreNonnullByDefault;
 
 public class KabloomFruitEntity extends ProjectileItemEntity {
     public KabloomFruitEntity(EntityType<? extends KabloomFruitEntity> entity, World world) {
@@ -41,34 +44,32 @@ public class KabloomFruitEntity extends ProjectileItemEntity {
     @OnlyIn(Dist.CLIENT)
     public void handleStatusUpdate(byte id) {
         if (id == 3) {
+            this.world.playSound(this.getPosX(), this.getPosY(), this.getPosZ(), BGSoundEvents.ENTITY_KABLOOM_FRUIT_EXPLODE.get(), SoundCategory.PLAYERS, 1.0F, (1.0F + (this.world.rand.nextFloat() - this.world.rand.nextFloat()) * 0.2F), true);
+
             for(int i = 0; i < 8; ++i) {
-                this.world.addParticle(new ItemParticleData(ParticleTypes.ITEM, this.getItem()), this.getPosX(), this.getPosY(), this.getPosZ(), ((double)this.rand.nextFloat() - 0.5D) * 0.08D, ((double)this.rand.nextFloat() - 0.5D) * 0.8D, ((double)this.rand.nextFloat() - 0.5D) * 0.08D);
+                this.world.addParticle(new ItemParticleData(ParticleTypes.ITEM, this.getItem()), this.getPosX(), this.getPosY(), this.getPosZ(), (this.rand.nextFloat() - 0.5D) * 0.08D, (this.rand.nextFloat() - 0.5D) * 0.8D, (this.rand.nextFloat() - 0.5D) * 0.08D);
+                this.world.addParticle(new RedstoneParticleData(0.50F, 0.8F, 0.53F, 1.0F), this.getPosX() + this.rand.nextDouble() / 2.0D * (rand.nextBoolean() ? 1 : -1), this.getPosY() + this.rand.nextDouble() / 2.0D * (rand.nextBoolean() ? 1 : -1), this.getPosZ() + this.rand.nextDouble() / 2.0D * (rand.nextBoolean() ? 1 : -1), 0.5D, 0.0D, 0.0D);
             }
+
+            this.world.addParticle(ParticleTypes.EXPLOSION, this.getPosX(), this.getPosY(), this.getPosZ(), 1.0D, 0.0D, 0.0D);
         }
     }
 
     @Override
-    @ParametersAreNonnullByDefault
     protected void onImpact(RayTraceResult result) {
         Vector3d vec = result.getHitVec();
+        if (this.world.getGameRules().get(GameRules.DO_ENTITY_DROPS).get()) {
+            ItemEntity item = new ItemEntity(this.world, vec.getX() + 0.5F * this.rand.nextDouble() * (this.rand.nextBoolean() ? 1 : -1), vec.getY() + 0.5F * this.rand.nextDouble(), vec.getZ() + 0.5F * this.rand.nextDouble() * (this.rand.nextBoolean() ? 1 : -1), new ItemStack(BGItems.KABLOOM_SEEDS.get()));
+            item.setDefaultPickupDelay();
+            this.world.addEntity(item);
+        }
 
-        ItemEntity item = new ItemEntity(this.world, vec.getX() + 0.5F * this.rand.nextDouble() * (this.rand.nextBoolean() ? 1 : -1), vec.getY() + 0.5F * this.rand.nextDouble(), vec.getZ() + 0.5F * this.rand.nextDouble() * (this.rand.nextBoolean() ? 1 : -1), new ItemStack(BGItems.KABLOOM_SEEDS.get()));
-        item.setDefaultPickupDelay();
-        this.world.addEntity(item);
-
-        createExplosion(vec.getX(), vec.getY(), vec.getZ());
+        createExplosion();
 
         if (!this.world.isRemote) {
             this.world.setEntityState(this, (byte) 3);
             this.remove();
         }
-    }
-
-    private void createExplosion(double x, double y, double z) {
-        Explosion kabloom = new Explosion(this.world, null, BGDamageSources.causeKabloomDamage(this, this.func_234616_v_()), null, x, y, z, 0.7F, false, Explosion.Mode.NONE);
-        kabloom.doExplosionA();
-        this.world.playSound(x, y, z, BGSoundEvents.ENTITY_KABLOOM_FRUIT_EXPLODE.get(), SoundCategory.PLAYERS, 1.0F, (1.0F + (this.world.rand.nextFloat() - this.world.rand.nextFloat()) * 0.2F), true);
-        this.world.addParticle(ParticleTypes.EXPLOSION, x, y, z, 1.0D, 0.0D, 0.0D);
     }
 
     @Override
@@ -84,5 +85,45 @@ public class KabloomFruitEntity extends ProjectileItemEntity {
     @Override
     public IPacket<?> createSpawnPacket() {
         return NetworkHooks.getEntitySpawningPacket(this);
+    }
+
+    private void createExplosion() {
+        Vector3d vector3d = this.getPositionVec();
+
+        for (Entity entity : this.world.getEntitiesWithinAABBExcludingEntity(null, this.getBoundingBox().grow(0.75D))) {
+            boolean flag = false;
+
+            for(int i = 0; i < 3; ++i) {
+                RayTraceResult raytraceresult = this.world.rayTraceBlocks(new RayTraceContext(vector3d, new Vector3d(entity.getPosX(), entity.getPosYHeight(0.5D * (double) i), entity.getPosZ()), RayTraceContext.BlockMode.COLLIDER, RayTraceContext.FluidMode.NONE, this));
+                if (raytraceresult.getType() == RayTraceResult.Type.MISS) {
+                    flag = true;
+                    break;
+                }
+            }
+
+            if (flag) {
+                float dmg = 0;
+                if (!entity.isImmuneToExplosions()) {
+                    double d5 = entity.getPosX() - this.getPosX();
+                    double d7 = entity.getPosYEye() - this.getPosY();
+                    double d9 = entity.getPosZ() - this.getPosZ();
+                    double d13 = MathHelper.sqrt(d5 * d5 + d7 * d7 + d9 * d9);
+                    if (d13 != 0.0D) {
+                        d5 = d5 / d13;
+                        d7 = d7 / d13;
+                        d9 = d9 / d13;
+                        double d10 = (this.getDistance(entity) > 1) ? 0.25D : 0.5D;
+                        dmg = 2.0F + 4.0F * ((float) d10);
+                        double d11 = dmg;
+                        if (entity instanceof LivingEntity) {
+                            d11 = ProtectionEnchantment.getBlastDamageReduction((LivingEntity) entity, d10);
+                        }
+                        entity.setMotion(entity.getMotion().add(d5 * d11, d7 * d11, d9 * d11));
+                    }
+                }
+
+                entity.attackEntityFrom(BGDamageSources.causeKabloomDamage(this, this.func_234616_v_()), dmg);
+            }
+        }
     }
 }
