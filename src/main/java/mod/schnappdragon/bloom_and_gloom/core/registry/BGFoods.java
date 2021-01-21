7a00268da7fe4511ab7bcd11680b86f86390426f
SnappyDragon64 <pg61151@gmail.com>
2021-01-21 19:51:43 +0530

    Saturation tweak

--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGFoods.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGFoods.java
@@ -4,5 +4,5 @@ import net.minecraft.item.Food;
 import net.minecraft.potion.EffectInstance;
 
 public class BGFoods {
-    public static final Food CURED_KABLOOM_FRUIT = (new Food.Builder()).hunger(3).saturation(0.2F).effect(() -> new EffectInstance(BGEffects.BLAST_RESISTANCE.get(), 1800, 0), 1.0F).build();
+    public static final Food CURED_KABLOOM_FRUIT = (new Food.Builder()).hunger(3).saturation(0.3F).effect(() -> new EffectInstance(BGEffects.BLAST_RESISTANCE.get(), 1800, 0), 1.0F).build();
 }
