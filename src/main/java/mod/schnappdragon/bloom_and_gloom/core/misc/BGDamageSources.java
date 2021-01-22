5ab129826f35681f5291f37d851fc62c49e5d4c4
SnappyDragon64 <pg61151@gmail.com>
2021-01-22 14:43:00 +0530

    Kabloom damage is not difficulty scaled

--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/misc/BGDamageSources.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/misc/BGDamageSources.java
@@ -9,6 +9,6 @@ import javax.annotation.Nullable;
 
 public class BGDamageSources {
     public static DamageSource causeKabloomDamage(KabloomFruitEntity kabloom, @Nullable Entity indirectEntityIn) {
-        return (new IndirectEntityDamageSource("bloom_and_gloom.kabloom", kabloom, indirectEntityIn)).setDifficultyScaled().setExplosion();
+        return (new IndirectEntityDamageSource("bloom_and_gloom.kabloom", kabloom, indirectEntityIn)).setExplosion();
     }
 }
