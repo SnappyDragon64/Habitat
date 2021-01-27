b620bf75c62b3a77c1dd56019b2f1f5334b5b531
SnappyDragon64 <pg61151@gmail.com>
2021-01-27 18:09:24 +0530

    Rafflesia excluded from bamboo jungles + BiomeDictionary

--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/event/BGGeneration.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/event/BGGeneration.java
@@ -2,22 +2,30 @@ package mod.schnappdragon.bloom_and_gloom.core.event;
 
 import mod.schnappdragon.bloom_and_gloom.core.BloomAndGloom;
 import mod.schnappdragon.bloom_and_gloom.core.registry.BGConfiguredFeatures;
+import net.minecraft.util.RegistryKey;
+import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
+import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
+import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = BloomAndGloom.MOD_ID)
 public class BGGeneration {
-    @SubscribeEvent
-    public static void addFeaturesToBiomes(BiomeLoadingEvent event) {
-        if (event.getCategory() == Biome.Category.JUNGLE)
-            event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, BGConfiguredFeatures.PATCH_RAFFLESIA);
-        else if (event.getCategory() == Biome.Category.PLAINS)
-            event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, BGConfiguredFeatures.PATCH_KABLOOM_BUSH);
+    @SubscribeEvent(priority =  EventPriority.HIGH)
+    public static void modifyBiomes(BiomeLoadingEvent event) {
+        RegistryKey<Biome> biomeRegistryKey = RegistryKey.getOrCreateKey(Registry.BIOME_KEY, event.getName());
 
-        if (event.getCategory() != Biome.Category.NETHER && event.getCategory() != Biome.Category.THEEND && event.getCategory() != Biome.Category.NONE)
+        if (BiomeDictionary.hasType(biomeRegistryKey, BiomeDictionary.Type.OVERWORLD)) {
             event.getGeneration().withFeature(GenerationStage.Decoration.UNDERGROUND_DECORATION, BGConfiguredFeatures.PATCH_SLIME_FERN);
+
+            if (event.getCategory() == Biome.Category.JUNGLE && !event.getName().getPath().contains("bamboo"))
+                event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, BGConfiguredFeatures.PATCH_RAFFLESIA);
+
+            if (event.getCategory() == Biome.Category.PLAINS)
+                event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, BGConfiguredFeatures.PATCH_KABLOOM_BUSH);
+        }
     }
 }
