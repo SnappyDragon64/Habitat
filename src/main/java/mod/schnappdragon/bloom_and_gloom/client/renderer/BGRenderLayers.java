ee1af3cc1c60360b950feae04e8034b586675820
SnappyDragon64 <pg61151@gmail.com>
2021-02-05 15:08:05 +0530

    Moved lines around

--- src/main/java/mod/schnappdragon/bloom_and_gloom/client/renderer/BGRenderLayers.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/client/renderer/BGRenderLayers.java
@@ -11,12 +11,13 @@ import net.minecraftforge.api.distmarker.OnlyIn;
 public class BGRenderLayers {
     public static void registerRenderLayers() {
         RenderTypeLookup.setRenderLayer(BGBlocks.RAFFLESIA.get(), RenderType.getCutout());
+        CompatHelper.registerCompat("quark", () -> RenderTypeLookup.setRenderLayer(BGBlocks.POTTED_RAFFLESIA.get(), RenderType.getCutout()));
+
         RenderTypeLookup.setRenderLayer(BGBlocks.KABLOOM_BUSH.get(), RenderType.getCutout());
+        CompatHelper.registerCompat("quark", () -> RenderTypeLookup.setRenderLayer(BGBlocks.POTTED_KABLOOM_BUSH.get(), RenderType.getCutout()));
+
         RenderTypeLookup.setRenderLayer(BGBlocks.SLIME_FERN.get(), RenderType.getCutout());
         RenderTypeLookup.setRenderLayer(BGBlocks.WALL_SLIME_FERN.get(), RenderType.getCutout());
-
-        CompatHelper.registerCompat("quark", () -> RenderTypeLookup.setRenderLayer(BGBlocks.POTTED_RAFFLESIA.get(), RenderType.getCutout()));
-        CompatHelper.registerCompat("quark", () -> RenderTypeLookup.setRenderLayer(BGBlocks.POTTED_KABLOOM_BUSH.get(), RenderType.getCutout()));
         RenderTypeLookup.setRenderLayer(BGBlocks.POTTED_SLIME_FERN.get(), RenderType.getCutout());
     }
 }
