43b88a61f8049060fe65582f00c325307d899a7f
SnappyDragon64 <pg61151@gmail.com>
2021-02-20 23:48:27 +0530

    Food effect changes

--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/misc/BGFoods.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/misc/BGFoods.java
@@ -5,7 +5,7 @@ import net.minecraft.item.Food;
 import net.minecraft.potion.EffectInstance;
 
 public class BGFoods {
-    public static final Food CANDIED_KABLOOM_FRUIT = new Food.Builder().hunger(3).saturation(0.3F).effect(() -> new EffectInstance(BGEffects.BLAST_ENDURANCE.get(), 1800, 0), 1.0F).build();
-    public static final Food ROASTED_CACTUS = new Food.Builder().hunger(5).saturation(0.6F).effect(() -> new EffectInstance(BGEffects.PRICKLING.get(), 2400, 0), 1.0F).build();
+    public static final Food CANDIED_KABLOOM_FRUIT = new Food.Builder().hunger(3).saturation(0.3F).effect(() -> new EffectInstance(BGEffects.BLAST_ENDURANCE.get(), 1200, 0), 1.0F).build();
+    public static final Food ROASTED_CACTUS = new Food.Builder().hunger(5).saturation(0.6F).build();
     public static final Food FAIRY_RING_MUSHROOM_STEW = new Food.Builder().hunger(6).saturation(0.6F).build();
 }
