fef6f8fd2a27b6244fb022544b3b87f965ec91c0
SnappyDragon64 <pg61151@gmail.com>
2021-02-21 01:41:48 +0530

    Potion registry crash fix

--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGPotions.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/registry/BGPotions.java
@@ -8,22 +8,24 @@ import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
 
+import java.util.function.Supplier;
+
 public class BGPotions {
     public static final DeferredRegister<Potion> POTIONS = DeferredRegister.create(ForgeRegistries.POTION_TYPES, BloomAndGloom.MOD_ID);
 
-    public static final RegistryObject<Potion> BLAST_ENDURANCE = register("blast_endurance", BGEffects.BLAST_ENDURANCE.get(), 3600);
-    public static final RegistryObject<Potion> LONG_BLAST_ENDURANCE = register("long_blast_endurance", BGEffects.BLAST_ENDURANCE.get(), 9600);
-    public static final RegistryObject<Potion> STRONG_BLAST_ENDURANCE = register("strong_blast_endurance", BGEffects.BLAST_ENDURANCE.get(), 1800, 1);
+    public static final RegistryObject<Potion> BLAST_ENDURANCE = register("blast_endurance", BGEffects.BLAST_ENDURANCE, 3600);
+    public static final RegistryObject<Potion> LONG_BLAST_ENDURANCE = register("long_blast_endurance", BGEffects.BLAST_ENDURANCE, 9600);
+    public static final RegistryObject<Potion> STRONG_BLAST_ENDURANCE = register("strong_blast_endurance", BGEffects.BLAST_ENDURANCE, 1800, 1);
 
-    public static final RegistryObject<Potion> PRICKLING = register("prickling", BGEffects.PRICKLING.get(), 3600);
-    public static final RegistryObject<Potion> LONG_PRICKLING = register("long_prickling", BGEffects.PRICKLING.get(), 9600);
-    public static final RegistryObject<Potion> STRONG_PRICKLING = register("strong_prickling", BGEffects.PRICKLING.get(), 1800, 1);
+    public static final RegistryObject<Potion> PRICKLING = register("prickling", BGEffects.PRICKLING, 3600);
+    public static final RegistryObject<Potion> LONG_PRICKLING = register("long_prickling", BGEffects.PRICKLING, 9600);
+    public static final RegistryObject<Potion> STRONG_PRICKLING = register("strong_prickling", BGEffects.PRICKLING, 1800, 1);
 
-    private static RegistryObject<Potion> register(String id, Effect effect, int duration) {
-        return POTIONS.register(id, () -> new Potion(new EffectInstance(effect, duration)));
+    private static RegistryObject<Potion> register(String id, Supplier<Effect> effect, int duration) {
+        return POTIONS.register(id, () -> new Potion(new EffectInstance(effect.get(), duration)));
     }
 
-    private static RegistryObject<Potion> register(String id, Effect effect, int duration, int amplifier) {
-        return POTIONS.register(id, () -> new Potion(new EffectInstance(effect, duration, amplifier)));
+    private static RegistryObject<Potion> register(String id, Supplier<Effect> effect, int duration, int amplifier) {
+        return POTIONS.register(id, () -> new Potion(new EffectInstance(effect.get(), duration, amplifier)));
     }
 }
