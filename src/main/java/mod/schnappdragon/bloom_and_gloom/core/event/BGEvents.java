501b3001b7a6dfbf193031d1be5baac1a5aabc78
SnappyDragon64 <pg61151@gmail.com>
2021-02-20 16:52:54 +0530

    Brown mooshrooms can make fairy ring mushroom stew

--- src/main/java/mod/schnappdragon/bloom_and_gloom/core/event/BGEvents.java
+++ src/main/java/mod/schnappdragon/bloom_and_gloom/core/event/BGEvents.java
@@ -3,11 +3,15 @@ package mod.schnappdragon.bloom_and_gloom.core.event;
 import mod.schnappdragon.bloom_and_gloom.common.entity.ai.goal.BGFindPollinationTargetGoal;
 import mod.schnappdragon.bloom_and_gloom.core.BloomAndGloom;
 import mod.schnappdragon.bloom_and_gloom.core.capabilities.classes.ConsumedFairyRingMushroom;
+import mod.schnappdragon.bloom_and_gloom.core.registry.BGItems;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.EntityType;
 import net.minecraft.entity.passive.BeeEntity;
 import net.minecraft.entity.passive.MooshroomEntity;
+import net.minecraft.item.ItemStack;
 import net.minecraft.item.Items;
+import net.minecraft.util.ActionResultType;
+import net.minecraft.util.DrinkHelper;
 import net.minecraftforge.event.entity.EntityJoinWorldEvent;
 import net.minecraftforge.event.entity.player.PlayerInteractEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
@@ -30,15 +34,30 @@ public class BGEvents {
     }
 
     /*
-     * Used to reset the ConsumedFairyRingMushroom capability when the mooshroom is milked.
+     * Used to add and modify mooshroom interaction with fairy ring mushrooms.
      */
 
     @SubscribeEvent
-    public static void resetMooshroomCapability(PlayerInteractEvent.EntityInteract event) {
-        if (event.getItemStack().getItem() == Items.BOWL && event.getTarget().getType() == EntityType.MOOSHROOM) {
+    public static void modifyMooshroomInteraction(PlayerInteractEvent.EntityInteractSpecific event) {
+        if (event.getTarget().getType() == EntityType.MOOSHROOM) {
             MooshroomEntity mooshroom = (MooshroomEntity) event.getTarget();
-            if (!mooshroom.isChild() && mooshroom.hasStewEffect != null)
-                mooshroom.getCapability(ConsumedFairyRingMushroom.Provider.CONSUMED_FAIRY_RING_MUSHROOM_CAPABILITY).resolve().get().setConsumedFairyRingMushroom(false);
+            if (!mooshroom.isChild()) {
+                if (event.getItemStack().getItem() == Items.BOWL) {
+                    boolean giveStew = false;
+
+                    if (mooshroom.hasStewEffect == null && mooshroom.getCapability(ConsumedFairyRingMushroom.Provider.CONSUMED_FAIRY_RING_MUSHROOM_CAPABILITY).resolve().get().getConsumedFairyRingMushroom()) {
+                        event.setCancellationResult(ActionResultType.SUCCESS);
+                        event.setCanceled(true);
+                        giveStew = true;
+                    }
+
+                    if (!event.getWorld().isRemote) {
+                        mooshroom.getCapability(ConsumedFairyRingMushroom.Provider.CONSUMED_FAIRY_RING_MUSHROOM_CAPABILITY).resolve().get().setConsumedFairyRingMushroom(false);
+                        if (giveStew)
+                            event.getPlayer().setHeldItem(event.getHand(), DrinkHelper.fill(event.getItemStack(), event.getPlayer(), new ItemStack(BGItems.FAIRY_RING_MUSHROOM_STEW.get()), false));
+                    }
+                }
+            }
         }
     }
 }
