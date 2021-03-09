313f95ad4f0063521f0af7a62b6d30233886f676
SnappyDragon64 <pg61151@gmail.com>
2021-03-10 01:01:26 +0530

    Update HabitatRenderLayers.java

--- src/main/java/mod/schnappdragon/habitat/client/renderer/HabitatRenderLayers.java
+++ src/main/java/mod/schnappdragon/habitat/client/renderer/HabitatRenderLayers.java
@@ -2,39 +2,44 @@ package mod.schnappdragon.habitat.client.renderer;
 
 import mod.schnappdragon.habitat.core.registry.HabitatBlocks;
 import mod.schnappdragon.habitat.core.util.CompatHelper;
+import net.minecraft.block.Block;
 import net.minecraft.client.renderer.RenderType;
 import net.minecraft.client.renderer.RenderTypeLookup;
 
 public class HabitatRenderLayers {
     public static void registerRenderLayers() {
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.RAFFLESIA.get(), RenderType.getCutout());
-        CompatHelper.registerCompat("quark", () -> RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_RAFFLESIA.get(), RenderType.getCutout()));
+        setRenderLayer(HabitatBlocks.RAFFLESIA.get(), RenderType.getCutout());
+        CompatHelper.registerCompat("quark", () -> setRenderLayer(HabitatBlocks.POTTED_RAFFLESIA.get(), RenderType.getCutout()));
 
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.KABLOOM_BUSH.get(), RenderType.getCutout());
-        CompatHelper.registerCompat("quark", () -> RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_KABLOOM_BUSH.get(), RenderType.getCutout()));
+        setRenderLayer(HabitatBlocks.KABLOOM_BUSH.get(), RenderType.getCutout());
+        CompatHelper.registerCompat("quark", () -> setRenderLayer(HabitatBlocks.POTTED_KABLOOM_BUSH.get(), RenderType.getCutout()));
 
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.SLIME_FERN.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.WALL_SLIME_FERN.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_SLIME_FERN.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.SLIME_FERN.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.WALL_SLIME_FERN.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_SLIME_FERN.get(), RenderType.getCutout());
 
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.ORANGE_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.PINK_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.RED_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.YELLOW_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.FLOWERING_ORANGE_BALL_CACTUS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.FLOWERING_PINK_BALL_CACTUS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.FLOWERING_RED_BALL_CACTUS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.FLOWERING_YELLOW_BALL_CACTUS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_ORANGE_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_PINK_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_RED_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_YELLOW_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_ORANGE_BALL_CACTUS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_PINK_BALL_CACTUS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_RED_BALL_CACTUS.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_YELLOW_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.ORANGE_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.PINK_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.RED_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.YELLOW_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.FLOWERING_ORANGE_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.FLOWERING_PINK_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.FLOWERING_RED_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.FLOWERING_YELLOW_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_ORANGE_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_PINK_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_RED_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_YELLOW_BALL_CACTUS_FLOWER.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_ORANGE_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_PINK_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_RED_BALL_CACTUS.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_YELLOW_BALL_CACTUS.get(), RenderType.getCutout());
 
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.FAIRY_RING_MUSHROOM.get(), RenderType.getCutout());
-        RenderTypeLookup.setRenderLayer(HabitatBlocks.POTTED_FAIRY_RING_MUSHROOM.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.FAIRY_RING_MUSHROOM.get(), RenderType.getCutout());
+        setRenderLayer(HabitatBlocks.POTTED_FAIRY_RING_MUSHROOM.get(), RenderType.getCutout());
+    }
+    
+    private static void setRenderLayer(Block block, RenderType type) {
+        RenderTypeLookup.setRenderLayer(block, type);
     }
 }
