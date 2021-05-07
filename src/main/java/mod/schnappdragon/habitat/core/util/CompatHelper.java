bf9a47ed81c535eaea3542002681839eb159e8e9
SnappyDragon64 <pg61151@gmail.com>
2021-05-07 13:15:41 +0530

    Unused imports

--- src/main/java/mod/schnappdragon/habitat/core/util/CompatHelper.java
+++ src/main/java/mod/schnappdragon/habitat/core/util/CompatHelper.java
@@ -1,12 +1,7 @@
 package mod.schnappdragon.habitat.core.util;
 
-import mod.schnappdragon.habitat.core.Habitat;
 import net.minecraftforge.fml.ModList;
-import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.fml.loading.FMLLoader;
-import net.minecraftforge.registries.IForgeRegistryEntry;
-
-import java.util.function.Supplier;
 
 public class CompatHelper {
     private static boolean modsLoaded(String... modids) {
