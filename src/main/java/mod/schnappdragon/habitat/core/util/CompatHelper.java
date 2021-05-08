cdfe846947db9a8a506b4194558469fb5368aab3
SnappyDragon64 <pg61151@gmail.com>
2021-05-08 16:29:38 +0530

    Update CompatHelper.java

--- src/main/java/mod/schnappdragon/habitat/core/util/CompatHelper.java
+++ src/main/java/mod/schnappdragon/habitat/core/util/CompatHelper.java
@@ -4,7 +4,7 @@ import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 
 public class CompatHelper {
-    private static boolean modsLoaded(String... modids) {
+    private static boolean checkCompat(String... modids) {
         if (FMLLoader.isProduction()) {
             ModList modList = ModList.get();
             for (String modid : modids) {
