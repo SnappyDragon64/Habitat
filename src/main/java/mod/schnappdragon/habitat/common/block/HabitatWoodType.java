d176a250a6ce947a16a475b53303907ac1efdcaf
SnappyDragon64 <pg61151@gmail.com>
2021-05-13 21:30:27 +0530

    WoodType AT + FRM WoodType

---
+++ src/main/java/mod/schnappdragon/habitat/common/block/HabitatWoodType.java
@@ -0,0 +1,7 @@
+package mod.schnappdragon.habitat.common.block;
+
+import net.minecraft.block.WoodType;
+
+public class HabitatWoodType {
+    public static final WoodType FAIRY_RING_MUSHROOM = WoodType.register(new WoodType("fairy_ring_mushroom"));
+}
