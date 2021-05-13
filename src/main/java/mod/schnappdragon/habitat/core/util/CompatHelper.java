2deac3045d3ed6cce8017faa46fea73ee22e701a
SnappyDragon64 <pg61151@gmail.com>
2021-05-13 21:30:40 +0530

    Helper method for itemgroup for compat items

--- src/main/java/mod/schnappdragon/habitat/core/util/CompatHelper.java
+++ src/main/java/mod/schnappdragon/habitat/core/util/CompatHelper.java
@@ -1,10 +1,11 @@
 package mod.schnappdragon.habitat.core.util;
 
+import net.minecraft.item.ItemGroup;
 import net.minecraftforge.fml.ModList;
 import net.minecraftforge.fml.loading.FMLLoader;
 
 public class CompatHelper {
-    private static boolean checkCompat(String... modids) {
+    public static boolean checkCompat(String... modids) {
         if (FMLLoader.isProduction()) {
             ModList modList = ModList.get();
             for (String modid : modids) {
@@ -13,5 +14,9 @@ public class CompatHelper {
             }
         }
         return true;
+    }
+
+    public static ItemGroup compatItemGroup(ItemGroup groupIn, String... modids) {
+        return checkCompat(modids) ? groupIn : null;
     }
 }
