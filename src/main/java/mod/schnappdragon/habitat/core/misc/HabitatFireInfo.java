7bbb53b9efde60c05887e7834375b971a3693f23
SnappyDragon64 <pg61151@gmail.com>
2021-05-22 15:36:56 +0530

    Update HabitatFireInfo.java

--- src/main/java/mod/schnappdragon/habitat/core/misc/HabitatFireInfo.java
+++ src/main/java/mod/schnappdragon/habitat/core/misc/HabitatFireInfo.java
@@ -17,6 +17,18 @@ public class HabitatFireInfo {
         setFireInfo(HabitatBlocks.PINK_BALL_CACTUS_FLOWER.get(), 60, 100);
         setFireInfo(HabitatBlocks.RED_BALL_CACTUS_FLOWER.get(), 60, 100);
         setFireInfo(HabitatBlocks.YELLOW_BALL_CACTUS_FLOWER.get(), 60, 100);
+
+        setFireInfo(HabitatBlocks.ENHANCED_FAIRY_RING_MUSHROOM_STEM.get(), 5, 5);
+        setFireInfo(HabitatBlocks.FAIRY_RING_MUSHROOM_HYPHAE.get(), 5, 5);
+        setFireInfo(HabitatBlocks.STRIPPED_FAIRY_RING_MUSHROOM_STEM.get(), 5, 5);
+        setFireInfo(HabitatBlocks.STRIPPED_FAIRY_RING_MUSHROOM_HYPHAE.get(), 5, 5);
+        setFireInfo(HabitatBlocks.FAIRY_RING_MUSHROOM_PLANKS.get(), 5, 20);
+        setFireInfo(HabitatBlocks.FAIRY_RING_MUSHROOM_SLAB.get(), 5, 20);
+        setFireInfo(HabitatBlocks.FAIRY_RING_MUSHROOM_STAIRS.get(), 5, 20);
+        setFireInfo(HabitatBlocks.FAIRY_RING_MUSHROOM_FENCE.get(), 5, 20);
+        setFireInfo(HabitatBlocks.FAIRY_RING_MUSHROOM_FENCE_GATE.get(), 5, 20);
+        setFireInfo(HabitatBlocks.FAIRY_RING_MUSHROOM_POST.get(), 5, 20);
+        setFireInfo(HabitatBlocks.STRIPPED_FAIRY_RING_MUSHROOM_POST.get(), 5, 20);
     }
 
     private static void setFireInfo(Block block, int encouragement, int flammability) {
