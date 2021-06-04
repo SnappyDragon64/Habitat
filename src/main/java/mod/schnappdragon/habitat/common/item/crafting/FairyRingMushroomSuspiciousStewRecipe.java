fd86abff3d4605b2439807cc961b5c9c7ea92acb
SnappyDragon64 <pg61151@gmail.com>
2021-06-04 23:30:59 +0530

    Random Suspicious Stew Recipe

--- src/main/java/mod/schnappdragon/habitat/common/item/crafting/FairyRingMushroomSuspiciousStewRecipe.java
+++ src/main/java/mod/schnappdragon/habitat/common/item/crafting/FairyRingMushroomSuspiciousStewRecipe.java
@@ -5,15 +5,16 @@ import mod.schnappdragon.habitat.core.registry.HabitatRecipeSerializers;
 import net.minecraft.block.Blocks;
 import net.minecraft.block.FlowerBlock;
 import net.minecraft.inventory.CraftingInventory;
-import net.minecraft.item.BlockItem;
-import net.minecraft.item.ItemStack;
-import net.minecraft.item.Items;
-import net.minecraft.item.SuspiciousStewItem;
+import net.minecraft.item.*;
 import net.minecraft.item.crafting.IRecipeSerializer;
 import net.minecraft.item.crafting.SpecialRecipe;
 import net.minecraft.tags.ItemTags;
 import net.minecraft.util.ResourceLocation;
 import net.minecraft.world.World;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
 
 public class FairyRingMushroomSuspiciousStewRecipe extends SpecialRecipe {
     public FairyRingMushroomSuspiciousStewRecipe(ResourceLocation idIn) {
@@ -25,10 +26,9 @@ public class FairyRingMushroomSuspiciousStewRecipe extends SpecialRecipe {
         boolean brown = false;
         boolean red = false;
         boolean fairy = false;
-        boolean flower = false;
         boolean bowl = false;
 
-        for(int i = 0; i < inv.getSizeInventory(); ++i) {
+        for (int i = 0; i < inv.getSizeInventory(); ++i) {
             ItemStack itemstack = inv.getStackInSlot(i);
             if (!itemstack.isEmpty()) {
                 if (itemstack.getItem() == Blocks.BROWN_MUSHROOM.asItem() && !brown)
@@ -37,8 +37,6 @@ public class FairyRingMushroomSuspiciousStewRecipe extends SpecialRecipe {
                     red = true;
                 else if (itemstack.getItem() == HabitatItems.FAIRY_RING_MUSHROOM.get() && !fairy)
                     fairy = true;
-                else if (itemstack.getItem().isIn(ItemTags.SMALL_FLOWERS) && !flower)
-                    flower = true;
                 else if (itemstack.getItem() == Items.BOWL && !bowl)
                     bowl = true;
                 else
@@ -46,33 +44,26 @@ public class FairyRingMushroomSuspiciousStewRecipe extends SpecialRecipe {
             }
         }
 
-        return brown && red && fairy && flower & bowl;
+        return brown && red && fairy && bowl;
     }
 
     @Override
     public ItemStack getCraftingResult(CraftingInventory inv) {
-        ItemStack flower = ItemStack.EMPTY;
+        List<Item> flowers = ItemTags.SMALL_FLOWERS.getAllElements().stream().filter((item) -> item instanceof BlockItem && ((BlockItem) item).getBlock() instanceof FlowerBlock).collect(Collectors.toList());
+        Optional<Item> flower = flowers.stream().skip((int) (flowers.size() * Math.random())).findFirst();
 
-        for(int i = 0; i < inv.getSizeInventory(); ++i) {
-            ItemStack inSlot = inv.getStackInSlot(i);
-            if (!inSlot.isEmpty() && inSlot.getItem().isIn(ItemTags.SMALL_FLOWERS)) {
-                flower = inSlot;
-                break;
-            }
+        if (flower.isPresent()) {
+            ItemStack stew = new ItemStack(Items.SUSPICIOUS_STEW, 1);
+            FlowerBlock flowerBlock = (FlowerBlock) ((BlockItem) flower.get()).getBlock();
+            SuspiciousStewItem.addEffect(stew, flowerBlock.getStewEffect(), flowerBlock.getStewEffectDuration() * 2);
+            return stew;
         }
-
-        ItemStack stew = new ItemStack(Items.SUSPICIOUS_STEW, 1);
-        if (flower.getItem() instanceof BlockItem && ((BlockItem) flower.getItem()).getBlock() instanceof FlowerBlock) {
-            FlowerBlock flowerblock = (FlowerBlock) ((BlockItem) flower.getItem()).getBlock();
-            SuspiciousStewItem.addEffect(stew, flowerblock.getStewEffect(), flowerblock.getStewEffectDuration() * 2);
-        }
-
-        return stew;
+        return ItemStack.EMPTY; // Reached when tag is empty or does not contain BlockItem for FlowerBlock at all
     }
 
     @Override
     public boolean canFit(int width, int height) {
-        return width >= 2 && height >= 3 || width >= 3 && height >= 2;
+        return width >= 2 && height >= 2;
     }
 
     @Override
