0626544f1144956dfd0eddcc24ee151a4ca98a85
SnappyDragon64 <pg61151@gmail.com>
2021-06-08 23:16:15 +0530

    Refactoring

--- src/main/java/mod/schnappdragon/habitat/core/event/world/BiomeModification.java
+++ src/main/java/mod/schnappdragon/habitat/core/event/world/BiomeModification.java
@@ -1,4 +1,4 @@
-package mod.schnappdragon.habitat.core.event;
+package mod.schnappdragon.habitat.core.event.world;
 
 import mod.schnappdragon.habitat.core.Habitat;
 import mod.schnappdragon.habitat.core.registry.HabitatConfiguredFeatures;
@@ -7,6 +7,7 @@ import net.minecraft.util.registry.Registry;
 import net.minecraft.world.biome.Biome;
 import net.minecraft.world.gen.GenerationStage;
 import net.minecraft.world.gen.feature.ConfiguredFeature;
+import net.minecraft.world.gen.feature.StructureFeature;
 import net.minecraftforge.common.BiomeDictionary;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
@@ -14,23 +15,23 @@ import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
 
 @Mod.EventBusSubscriber(modid = Habitat.MOD_ID)
-public class HabitatGeneration {
+public class BiomeModification {
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void modifyBiomes(BiomeLoadingEvent event) {
         if (event.getName() != null) {
             RegistryKey<Biome> biomeRegistryKey = RegistryKey.getOrCreateKey(Registry.BIOME_KEY, event.getName());
-            FeatureHelper helper = new FeatureHelper(event);
+            ModificationHelper helper = new ModificationHelper(event);
 
             if (BiomeDictionary.hasType(biomeRegistryKey, BiomeDictionary.Type.OVERWORLD)) {
                 helper.addFeature(HabitatConfiguredFeatures.PATCH_SLIME_FERN, GenerationStage.Decoration.UNDERGROUND_DECORATION);
 
                 // Jungles excluding Bamboo Jungles
                 if (event.getCategory() == Biome.Category.JUNGLE && !event.getName().getPath().contains("bamboo"))
-                    helper.addVegetalDecorationFeature(HabitatConfiguredFeatures.PATCH_RAFFLESIA);
+                    helper.addFeature(HabitatConfiguredFeatures.PATCH_RAFFLESIA, GenerationStage.Decoration.VEGETAL_DECORATION);
 
                 // Plains
                 if (event.getCategory() == Biome.Category.PLAINS)
-                    helper.addVegetalDecorationFeature(HabitatConfiguredFeatures.PATCH_KABLOOM_BUSH);
+                    helper.addFeature(HabitatConfiguredFeatures.PATCH_KABLOOM_BUSH, GenerationStage.Decoration.VEGETAL_DECORATION);
 
                 // Dark Forests
                 if (event.getName().getPath().contains("dark_forest"))
@@ -38,24 +39,24 @@ public class HabitatGeneration {
 
                 // Desert and Badlands
                 if (event.getCategory() == Biome.Category.DESERT || event.getCategory() == Biome.Category.MESA)
-                    helper.addVegetalDecorationFeature(HabitatConfiguredFeatures.PATCH_BALL_CACTUS);
+                    helper.addFeature(HabitatConfiguredFeatures.PATCH_BALL_CACTUS, GenerationStage.Decoration.VEGETAL_DECORATION);
             }
         }
     }
 
-    private static class FeatureHelper {
+    private static class ModificationHelper {
         private static BiomeLoadingEvent event;
 
-        private FeatureHelper(BiomeLoadingEvent event) {
-            FeatureHelper.event = event;
+        private ModificationHelper(BiomeLoadingEvent event) {
+            ModificationHelper.event = event;
         }
 
         private void addFeature(ConfiguredFeature<?, ?> feature, GenerationStage.Decoration stage) {
             event.getGeneration().withFeature(stage, feature);
         }
 
-        private void addVegetalDecorationFeature(ConfiguredFeature<?, ?> feature) {
-            event.getGeneration().withFeature(GenerationStage.Decoration.VEGETAL_DECORATION, feature);
+        private void addStructure(StructureFeature<?, ?> structure) {
+            event.getGeneration().getStructures().add(() -> structure);
         }
     }
 }
