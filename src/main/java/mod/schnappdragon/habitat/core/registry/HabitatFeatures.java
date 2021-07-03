d3a7265bc4768aacb665828d2ae170d5d93b723a
SnappyDragon64 <pg61151@gmail.com>
2021-07-03 12:24:58 +0530

    Removed Fairy Ring Feature

--- src/main/java/mod/schnappdragon/habitat/core/registry/HabitatFeatures.java
+++ src/main/java/mod/schnappdragon/habitat/core/registry/HabitatFeatures.java
@@ -1,13 +1,11 @@
 package mod.schnappdragon.habitat.core.registry;
 
 import mod.schnappdragon.habitat.common.world.gen.feature.BigFairyRingMushroomFeature;
-import mod.schnappdragon.habitat.common.world.gen.feature.FairyRingFeature;
 import mod.schnappdragon.habitat.common.world.gen.feature.SlimeFernFeature;
 import mod.schnappdragon.habitat.core.Habitat;
 import net.minecraft.world.gen.feature.BigMushroomFeatureConfig;
 import net.minecraft.world.gen.feature.BlockClusterFeatureConfig;
 import net.minecraft.world.gen.feature.Feature;
-import net.minecraft.world.gen.feature.NoFeatureConfig;
 import net.minecraftforge.fml.RegistryObject;
 import net.minecraftforge.registries.DeferredRegister;
 import net.minecraftforge.registries.ForgeRegistries;
@@ -17,9 +15,6 @@ public class HabitatFeatures {
 
     public final static RegistryObject<Feature<BlockClusterFeatureConfig>> SLIME_FERN_FEATURE = FEATURES.register("slime_fern",
             () -> new SlimeFernFeature(BlockClusterFeatureConfig.field_236587_a_));
-
-    public final static RegistryObject<Feature<NoFeatureConfig>> FAIRY_RING_FEATURE = FEATURES.register("fairy_ring",
-            () -> new FairyRingFeature(NoFeatureConfig.field_236558_a_));
 
     public final static RegistryObject<Feature<BigMushroomFeatureConfig>> HUGE_FAIRY_RING_MUSHROOM_FEATURE = FEATURES.register("huge_fairy_ring_mushroom",
             () -> new BigFairyRingMushroomFeature(BigMushroomFeatureConfig.CODEC));
