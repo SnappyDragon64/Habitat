fbefc7d77bc510544ef8ac68f2cdd0972a2d7cc9
SnappyDragon18 <pg61151@gmail.com>
2021-10-23 12:48:23 +0530

    Added another tag for TemptGoal item

--- src/main/java/mod/schnappdragon/habitat/core/tags/HabitatItemTags.java
+++ src/main/java/mod/schnappdragon/habitat/core/tags/HabitatItemTags.java
@@ -10,7 +10,8 @@ public class HabitatItemTags {
     public static final INamedTag<Item> BALL_CACTI = makeTag("ball_cacti");
     public static final INamedTag<Item> FAIRY_RING_MUSHROOM_STEMS = makeTag("fairy_ring_mushroom_stems");
     public static final INamedTag<Item> POOKA_BREEDING_FOOD = makeTag("pooka_breeding_food");
-    public static final INamedTag<Item> POOKA_PACIFICATION_FOOD = makeTag("pooka_pacification_food");
+    public static final INamedTag<Item> POOKA_FEEDING_FOOD = makeTag("pooka_feeding_food");
+    public static final INamedTag<Item> POOKA_FOOD = makeTag("pooka_food");
 
     private static INamedTag<Item> makeTag(String id) {
         return ItemTags.makeWrapperTag(Habitat.MOD_ID + ":" + id);
