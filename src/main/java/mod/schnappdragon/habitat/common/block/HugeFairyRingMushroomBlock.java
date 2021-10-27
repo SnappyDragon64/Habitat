2e853a6b5614ad8910c3824d8d787a7d35e85e1c
SnappyDragon18 <pg61151@gmail.com>
2021-10-27 14:48:37 +0530

    Walking on Fairy Ring Mushroom Block converts Rabbits

--- src/main/java/mod/schnappdragon/habitat/common/block/HugeFairyRingMushroomBlock.java
+++ src/main/java/mod/schnappdragon/habitat/common/block/HugeFairyRingMushroomBlock.java
@@ -1,16 +1,39 @@
 package mod.schnappdragon.habitat.common.block;
 
+import mod.schnappdragon.habitat.common.entity.monster.PookaEntity;
 import mod.schnappdragon.habitat.core.registry.HabitatParticleTypes;
+import mod.schnappdragon.habitat.core.registry.HabitatSoundEvents;
 import net.minecraft.block.BlockState;
 import net.minecraft.block.HugeMushroomBlock;
+import net.minecraft.entity.Entity;
+import net.minecraft.entity.EntityType;
+import net.minecraft.entity.passive.RabbitEntity;
 import net.minecraft.util.math.BlockPos;
 import net.minecraft.world.World;
+import net.minecraft.world.server.ServerWorld;
 
 import java.util.Random;
 
 public class HugeFairyRingMushroomBlock extends HugeMushroomBlock {
     public HugeFairyRingMushroomBlock(Properties properties) {
         super(properties);
+    }
+
+    /*
+     * Conversion Method
+     */
+
+    @Override
+    public void onEntityWalk(World worldIn, BlockPos pos, Entity entityIn) {
+        if (entityIn.getType() == EntityType.RABBIT) {
+            RabbitEntity rabbit = (RabbitEntity) entityIn;
+            rabbit.playSound(HabitatSoundEvents.ENTITY_RABBIT_CONVERTED_TO_POOKA.get(), 1.0F, rabbit.isChild() ? (rabbit.getRNG().nextFloat() - rabbit.getRNG().nextFloat()) * 0.2F + 1.5F : (rabbit.getRNG().nextFloat() - rabbit.getRNG().nextFloat()) * 0.2F + 1.0F);
+            rabbit.remove();
+            worldIn.addEntity(PookaEntity.convertRabbit(rabbit));
+
+            for (int i = 0; i < 8; i++)
+                ((ServerWorld) worldIn).spawnParticle(HabitatParticleTypes.FAIRY_RING_SPORE.get(), rabbit.getPosXRandom(0.5D), rabbit.getPosYHeight(0.5D), rabbit.getPosZRandom(0.5D), 0, rabbit.getRNG().nextGaussian(), 0.0D, rabbit.getRNG().nextGaussian(), 0.01D);
+        }
     }
 
     /*
