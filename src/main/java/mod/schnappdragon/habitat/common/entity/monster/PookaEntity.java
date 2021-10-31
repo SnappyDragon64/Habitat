a897eec92a9ede1a33cb851f6eb67e3411569964
SnappyDragon18 <pg61151@gmail.com>
2021-10-31 19:58:42 +0530

    Fixed Pooka conversion

--- src/main/java/mod/schnappdragon/habitat/common/entity/monster/PookaEntity.java
+++ src/main/java/mod/schnappdragon/habitat/common/entity/monster/PookaEntity.java
@@ -446,21 +446,23 @@ public class PookaEntity extends RabbitEntity implements IMob, IForgeShearable {
      */
 
     public boolean attackEntityAsMob(Entity entityIn) {
+        if (entityIn.getType() == EntityType.RABBIT && !entityIn.isInvulnerableTo(DamageSource.causeMobDamage(this))) {
+            this.playSound(HabitatSoundEvents.ENTITY_POOKA_ATTACK.get(), 1.0F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
+
+            RabbitEntity rabbit = (RabbitEntity) entityIn;
+            rabbit.playSound(HabitatSoundEvents.ENTITY_RABBIT_CONVERTED_TO_POOKA.get(), 1.0F, rabbit.isChild() ? (rabbit.getRNG().nextFloat() - rabbit.getRNG().nextFloat()) * 0.2F + 1.5F : (rabbit.getRNG().nextFloat() - rabbit.getRNG().nextFloat()) * 0.2F + 1.0F);
+            rabbit.remove();
+            this.world.addEntity(convertRabbit(rabbit));
+
+            for (int i = 0; i < 8; i++)
+                ((ServerWorld) this.world).spawnParticle(HabitatParticleTypes.FAIRY_RING_SPORE.get(), rabbit.getPosXRandom(0.5D), rabbit.getPosYHeight(0.5D), rabbit.getPosZRandom(0.5D), 0, rabbit.getRNG().nextGaussian(), 0.0D, rabbit.getRNG().nextGaussian(), 0.01D);
+            return false;
+        }
+
         boolean flag = entityIn.attackEntityFrom(DamageSource.causeMobDamage(this), (float) this.getAttributeValue(Attributes.ATTACK_DAMAGE));
         if (flag) {
             this.applyEnchantments(this, entityIn);
             this.playSound(HabitatSoundEvents.ENTITY_POOKA_ATTACK.get(), 1.0F, (this.rand.nextFloat() - this.rand.nextFloat()) * 0.2F + 1.0F);
-
-            if (entityIn.getType() == EntityType.RABBIT) {
-                RabbitEntity rabbit = (RabbitEntity) entityIn;
-                rabbit.playSound(HabitatSoundEvents.ENTITY_RABBIT_CONVERTED_TO_POOKA.get(), 1.0F, rabbit.isChild() ? (rabbit.getRNG().nextFloat() - rabbit.getRNG().nextFloat()) * 0.2F + 1.5F : (rabbit.getRNG().nextFloat() - rabbit.getRNG().nextFloat()) * 0.2F + 1.0F);
-                rabbit.remove();
-                this.world.addEntity(convertRabbit(rabbit));
-
-                for (int i = 0; i < 8; i++)
-                    ((ServerWorld) this.world).spawnParticle(HabitatParticleTypes.FAIRY_RING_SPORE.get(), rabbit.getPosXRandom(0.5D), rabbit.getPosYHeight(0.5D), rabbit.getPosZRandom(0.5D), 0, rabbit.getRNG().nextGaussian(), 0.0D, rabbit.getRNG().nextGaussian(), 0.01D);
-                return false;
-            }
 
             if (!this.isChild() && entityIn instanceof LivingEntity) {
                 Effect effect = Effect.get(ailmentId);
