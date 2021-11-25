ebcdf4b102c36cf36df1ce8a49c0c5a8c7ae4fcb
SnappyDragon18 <pg61151@gmail.com>
2021-11-25 20:02:03 +0530

    Config

--- src/main/java/mod/schnappdragon/habitat/core/HabitatConfig.java
+++ src/main/java/mod/schnappdragon/habitat/core/HabitatConfig.java
@@ -34,19 +34,16 @@ public class HabitatConfig {
         Common(ForgeConfigSpec.Builder builder) {
             builder.comment("Common Configurations for Habitat").push("common");
 
-            builder.comment("\n__MISC__\n");
-            builder.comment("\nFairy Ring Mushroom Stew Effects\nEnter them in the format effectid:duration (duration in seconds) and separate them with a single ,");
+            builder.comment("\n== MISC ==\n\nFairy Ring Mushroom Stew Effects\nEnter them in the format effectid:duration (duration in seconds) and separate them with a single ,");
             suspiciousStewEffects = builder.define("suspicious_stew_effects", "1:12,5:8,8:16,10:10,11:8,2:8,15:12,17:16,18:8,19:10");
 
-            builder.comment("\n__MOBS__\n");
-            builder.comment("\n_POOKA_\nCooldown duration after Pooka gives the player it is following an effect. (duration in seconds)");
+            builder.comment("\n== MOBS ==\n\n= POOKA =\nCooldown duration after Pooka gives the player it is following an effect. (duration in seconds)");
             pookaAidCooldown = builder.define("pooka_aid_cooldown", 20);
-            builder.comment("Effects\nEnter them in the format effectid:duration (duration in seconds) and separate them with a single ,");
+            builder.comment("Pooka Effects\nEnter them in the format effectid:duration (duration in seconds) and separate them with a single ,");
             pookaPositiveEffects = builder.define("pooka_positive_effects", "1:6,5:4,8:8,10:5,11:4");
             pookaNegativeEffects = builder.define("pooka_negative_effects", "2:4,15:6,17:8,18:4,19:5");
 
-            builder.comment("\n__WORLD GEN__\n");
-            builder.comment("\nFeatures have 1 in X Chance of Generating per Chunk where X is the Config Value");
+            builder.comment("\n== WORLD GEN ==\n\nFeatures have 1 in X Chance of Generating per Chunk where X is the Config Value");
             rafflesiaChance = builder.define("rafflesia_chance", 3);
             kabloomBushChance = builder.define("kabloom_bush_chance", 144);
             slimeFernChance = builder.define("slime_fern_chance", 2);
