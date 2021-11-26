13700f96a873f3509d138d486be16c87eb581526
SnappyDragon18 <pg61151@gmail.com>
2021-11-27 00:50:41 +0530

    Unused import

--- src/main/java/mod/schnappdragon/habitat/common/entity/monster/Pooka.java
+++ src/main/java/mod/schnappdragon/habitat/common/entity/monster/Pooka.java
@@ -37,7 +37,6 @@ import net.minecraft.world.entity.monster.Enemy;
 import net.minecraft.world.entity.player.Player;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.Ingredient;
-import net.minecraft.world.level.GameRules;
 import net.minecraft.world.level.Level;
 import net.minecraft.world.level.LevelAccessor;
 import net.minecraft.world.level.ServerLevelAccessor;
