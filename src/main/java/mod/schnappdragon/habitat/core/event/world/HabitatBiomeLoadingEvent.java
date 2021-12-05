d6e68f545c379efd2dde5510a2ff59a4dc55544d
SnappyDragon18 <pg61151@gmail.com>
2021-12-05 16:28:30 +0530

    Update HabitatBiomeLoadingEvent.java

--- src/main/java/mod/schnappdragon/habitat/core/event/world/HabitatBiomeLoadingEvent.java
+++ src/main/java/mod/schnappdragon/habitat/core/event/world/HabitatBiomeLoadingEvent.java
@@ -6,7 +6,6 @@ import mod.schnappdragon.habitat.core.registry.HabitatConfiguredStructures;
 import mod.schnappdragon.habitat.core.registry.HabitatEntityTypes;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
@@ -29,38 +28,57 @@ public class HabitatBiomeLoadingEvent {
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void modifyBiomes(BiomeLoadingEvent event) {
         if (event.getName() != null) {
-            ResourceKey<Biome> biome = ResourceKey.create(Registry.BIOME_REGISTRY, event.getName());
+            ModificationHelper helper = new ModificationHelper(event);
 
-            if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.OVERWORLD)) {
-                ModificationHelper helper = new ModificationHelper(event);
-
-                // All biomes
+            if (helper.checkType(BiomeDictionary.Type.OVERWORLD)) {
+                // All Biomes
                 helper.addFeature(HabitatConfiguredFeatures.PATCH_SLIME_FERN, GenerationStep.Decoration.UNDERGROUND_DECORATION);
 
-                // Jungles
-                if (helper.checkKeys(Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE)) {
-                    // Excluding Bamboo Jungles
-                    if (!helper.checkKeys(Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS))
-                        helper.addFeature(HabitatConfiguredFeatures.PATCH_RAFFLESIA, GenerationStep.Decoration.VEGETAL_DECORATION);
+                // Deserts and Badlands
+                if (helper.checkKey(Biomes.DESERT, Biomes.DESERT_HILLS, Biomes.BADLANDS, Biomes.BADLANDS_PLATEAU, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_BADLANDS_PLATEAU, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.WOODED_BADLANDS_PLATEAU)) {
+                    // Wooded Badlands
+                    if (helper.checkKey(Biomes.WOODED_BADLANDS_PLATEAU, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU))
+                        helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 20, 3, 4);
+
+                    helper.addFeature(HabitatConfiguredFeatures.PATCH_BALL_CACTUS, GenerationStep.Decoration.VEGETAL_DECORATION);
                 }
+
+                // Forest Biomes
+                if (helper.checkCategory(Biome.BiomeCategory.FOREST)) {
+                    // Dark Forests
+                    if (helper.checkKey(Biomes.DARK_FOREST, Biomes.DARK_FOREST_HILLS))
+                        helper.addStructure(HabitatConfiguredStructures.FAIRY_RING);
+                    // Flower Forests
+                    else if (helper.checkKey(Biomes.FLOWER_FOREST))
+                        helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 20, 3, 4);
+                    // Other Non-Spooky and Non-Dead Forests
+                    else if (!helper.checkType(BiomeDictionary.Type.SPOOKY, BiomeDictionary.Type.DEAD))
+                        helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 3, 3, 4);
+                }
+
+                // Vanilla Jungle Biomes
+                if (helper.checkKey(Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE)) {
+                    // Excluding Bamboo Jungles
+                    if (!helper.checkKey(Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS))
+                        helper.addFeature(HabitatConfiguredFeatures.PATCH_RAFFLESIA, GenerationStep.Decoration.VEGETAL_DECORATION);
+
+                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 20, 3, 4);
+                }
+                // Modded Jungle Biomes
+                else if (helper.checkCategory(Biome.BiomeCategory.JUNGLE))
+                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 3, 3, 4);
 
                 // Plains
                 if (helper.checkCategory(Biome.BiomeCategory.PLAINS))
                     helper.addFeature(HabitatConfiguredFeatures.PATCH_KABLOOM_BUSH, GenerationStep.Decoration.VEGETAL_DECORATION);
 
-                // Deserts and Badlands
-                if (helper.checkKeys(Biomes.DESERT, Biomes.DESERT_HILLS, Biomes.BADLANDS, Biomes.BADLANDS_PLATEAU, Biomes.ERODED_BADLANDS, Biomes.MODIFIED_BADLANDS_PLATEAU, Biomes.MODIFIED_WOODED_BADLANDS_PLATEAU, Biomes.WOODED_BADLANDS_PLATEAU))
-                    helper.addFeature(HabitatConfiguredFeatures.PATCH_BALL_CACTUS, GenerationStep.Decoration.VEGETAL_DECORATION);
+                // Savannas
+                if (helper.checkCategory(Biome.BiomeCategory.SAVANNA))
+                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 3, 3, 4);
 
-                // Dark Forests
-                if (helper.checkKeys(Biomes.DARK_FOREST, Biomes.DARK_FOREST_HILLS))
-                    helper.addStructure(HabitatConfiguredStructures.FAIRY_RING);
-
-                // WIP!
-                if (helper.checkKeys(Biomes.FLOWER_FOREST, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE))
-                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 20, 3, 4);
-                else if (helper.checkCategories(Biome.BiomeCategory.FOREST, Biome.BiomeCategory.TAIGA, Biome.BiomeCategory.SAVANNA, Biome.BiomeCategory.JUNGLE))
-                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 4, 3, 4);
+                // Taigas
+                if (helper.checkCategory(Biome.BiomeCategory.TAIGA))
+                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 3, 3, 4);
             }
         }
     }
@@ -69,34 +87,36 @@ public class HabitatBiomeLoadingEvent {
         private static BiomeLoadingEvent event;
         private static BiomeGenerationSettingsBuilder generation;
         private static MobSpawnInfoBuilder spawns;
+        ResourceKey<Biome> biome;
 
         private ModificationHelper(BiomeLoadingEvent event) {
             ModificationHelper.event = event;
             generation = event.getGeneration();
             spawns = event.getSpawns();
+            biome = ResourceKey.create(Registry.BIOME_REGISTRY, event.getName());
         }
 
-        private boolean isVanilla() {
-            return ResourceLocation.DEFAULT_NAMESPACE.equals(event.getName().getNamespace());
-        }
-
-        private boolean checkCategory(Biome.BiomeCategory category) {
-            return event.getCategory() == category;
-        }
-
-        private boolean checkCategories(Biome.BiomeCategory... categories) {
+        private boolean checkCategory(Biome.BiomeCategory... categories) {
             for (Biome.BiomeCategory category : categories) {
-                if (this.checkCategory(category)) {
+                if (event.getCategory() == category)
                     return true;
-                }
             }
 
             return false;
         }
 
-        private boolean checkKeys(ResourceKey<?>... biomes) {
+        private boolean checkKey(ResourceKey<?>... biomes) {
             for (ResourceKey<?> biome : biomes) {
                 if (biome.location().equals(event.getName()))
+                    return true;
+            }
+
+            return false;
+        }
+
+        private boolean checkType(BiomeDictionary.Type... types) {
+            for (BiomeDictionary.Type type : types) {
+                if (BiomeDictionary.hasType(biome, type))
                     return true;
             }
 
