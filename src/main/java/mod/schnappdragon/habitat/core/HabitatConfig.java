b6489e892c2e50ed3d9a11ab4d11e8baffb6b9a5
SnappyDragon18 <pg61151@gmail.com>
2021-12-05 13:23:22 +0530

    Removed most world gen configs (WIP)

--- src/main/java/mod/schnappdragon/habitat/core/HabitatConfig.java
+++ src/main/java/mod/schnappdragon/habitat/core/HabitatConfig.java
@@ -14,18 +14,6 @@ public class HabitatConfig {
         public final ForgeConfigSpec.ConfigValue<Integer> fairyRingAverage;
         public final ForgeConfigSpec.ConfigValue<Integer> fairyRingMinimum;
 
-        public final ForgeConfigSpec.ConfigValue<String> rafflesiaWhitelist;
-        public final ForgeConfigSpec.ConfigValue<String> kabloomBushWhitelist;
-        public final ForgeConfigSpec.ConfigValue<String> slimeFernWhitelist;
-        public final ForgeConfigSpec.ConfigValue<String> ballCactusWhitelist;
-        public final ForgeConfigSpec.ConfigValue<String> fairyRingWhitelist;
-
-        public final ForgeConfigSpec.ConfigValue<String> rafflesiaBlacklist;
-        public final ForgeConfigSpec.ConfigValue<String> kabloomBushBlacklist;
-        public final ForgeConfigSpec.ConfigValue<String> slimeFernBlacklist;
-        public final ForgeConfigSpec.ConfigValue<String> ballCactusBlacklist;
-        public final ForgeConfigSpec.ConfigValue<String> fairyRingBlacklist;
-
         Common(ForgeConfigSpec.Builder builder) {
             builder.comment("Common Configurations for Habitat").push("common");
 
@@ -38,23 +26,9 @@ public class HabitatConfig {
             pookaPositiveEffects = builder.define("pooka_positive_effects", "1:6,5:4,8:8,10:5,11:4");
             pookaNegativeEffects = builder.define("pooka_negative_effects", "2:4,15:6,17:8,18:4,19:5");
 
-            builder.comment("Average and Minimum Chunk Spacing for Structures (Minimum must be lower than Average)");
+            builder.comment("\n== WORLD GEN ==\n\nAverage and Minimum Chunk Spacing for Structures (Minimum must be lower than Average, 1001 to disable)");
             fairyRingAverage = builder.define("fairy_ring_average", 24);
             fairyRingMinimum = builder.define("fairy_ring_minimum", 4);
-
-            builder.comment("Whitelist and Blacklist for Biomes in which Features and Structures can generate\nUse the Biome Name with Namespace (namespace:biome) and separate Biomes if more than one with a single ,\nBiome Whitelist");
-            rafflesiaWhitelist = builder.define("rafflesia_whitelist", "");
-            kabloomBushWhitelist = builder.define("kabloom_bush_whitelist", "");
-            slimeFernWhitelist = builder.define("slime_fern_whitelist", "");
-            ballCactusWhitelist = builder.define("ball_cactus_whitelist", "");
-            fairyRingWhitelist = builder.define("fairy_ring_whitelist", "");
-
-            builder.comment("Biome Blacklist");
-            rafflesiaBlacklist = builder.define("rafflesia_blacklist", "");
-            kabloomBushBlacklist = builder.define("kabloom_bush_blacklist", "");
-            slimeFernBlacklist = builder.define("slime_fern_blacklist", "");
-            ballCactusBlacklist = builder.define("ball_cactus_blacklist", "");
-            fairyRingBlacklist = builder.define("fairy_ring_blacklist", "");
 
             builder.pop();
         }
--- src/main/java/mod/schnappdragon/habitat/core/event/world/HabitatBiomeLoadingEvent.java
+++ src/main/java/mod/schnappdragon/habitat/core/event/world/HabitatBiomeLoadingEvent.java
@@ -1,12 +1,12 @@
 package mod.schnappdragon.habitat.core.event.world;
 
 import mod.schnappdragon.habitat.core.Habitat;
-import mod.schnappdragon.habitat.core.HabitatConfig;
 import mod.schnappdragon.habitat.core.registry.HabitatConfiguredFeatures;
 import mod.schnappdragon.habitat.core.registry.HabitatConfiguredStructures;
 import mod.schnappdragon.habitat.core.registry.HabitatEntityTypes;
 import net.minecraft.core.Registry;
 import net.minecraft.resources.ResourceKey;
+import net.minecraft.resources.ResourceLocation;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraft.world.level.biome.Biome;
@@ -16,45 +16,50 @@ import net.minecraft.world.level.levelgen.GenerationStep;
 import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.ConfiguredStructureFeature;
 import net.minecraftforge.common.BiomeDictionary;
-import net.minecraftforge.common.ForgeConfigSpec;
 import net.minecraftforge.common.world.BiomeGenerationSettingsBuilder;
 import net.minecraftforge.common.world.MobSpawnInfoBuilder;
 import net.minecraftforge.event.world.BiomeLoadingEvent;
 import net.minecraftforge.eventbus.api.EventPriority;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import org.apache.commons.lang3.StringUtils;
-
-import java.util.Arrays;
-import java.util.List;
 
 @Mod.EventBusSubscriber(modid = Habitat.MODID)
 public class HabitatBiomeLoadingEvent {
 
     @SubscribeEvent(priority = EventPriority.HIGH)
     public static void modifyBiomes(BiomeLoadingEvent event) {
-        if (event.getName() != null && BiomeDictionary.hasType(ResourceKey.create(Registry.BIOME_REGISTRY, event.getName()), BiomeDictionary.Type.OVERWORLD)) {
-            ModificationHelper helper = new ModificationHelper(event);
+        if (event.getName() != null) {
+            ResourceKey<Biome> biome = ResourceKey.create(Registry.BIOME_REGISTRY, event.getName());
 
-            if (helper.check(helper.checkCategory(Biome.BiomeCategory.JUNGLE) && !helper.checkKeys(Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS), HabitatConfig.COMMON.rafflesiaWhitelist, HabitatConfig.COMMON.rafflesiaBlacklist))
-                helper.addFeature(HabitatConfiguredFeatures.PATCH_RAFFLESIA, GenerationStep.Decoration.VEGETAL_DECORATION);
+            if (BiomeDictionary.hasType(biome, BiomeDictionary.Type.OVERWORLD)) {
+                ModificationHelper helper = new ModificationHelper(event);
 
-            if (helper.check(helper.checkCategory(Biome.BiomeCategory.PLAINS), HabitatConfig.COMMON.kabloomBushWhitelist, HabitatConfig.COMMON.kabloomBushBlacklist))
-                helper.addFeature(HabitatConfiguredFeatures.PATCH_KABLOOM_BUSH, GenerationStep.Decoration.VEGETAL_DECORATION);
-
-            if (helper.check(HabitatConfig.COMMON.slimeFernWhitelist, HabitatConfig.COMMON.slimeFernBlacklist))
                 helper.addFeature(HabitatConfiguredFeatures.PATCH_SLIME_FERN, GenerationStep.Decoration.UNDERGROUND_DECORATION);
 
-            if (helper.check(helper.checkCategory(Biome.BiomeCategory.DESERT) || helper.checkCategory(Biome.BiomeCategory.MESA), HabitatConfig.COMMON.ballCactusWhitelist, HabitatConfig.COMMON.ballCactusBlacklist))
-                helper.addFeature(HabitatConfiguredFeatures.PATCH_BALL_CACTUS, GenerationStep.Decoration.VEGETAL_DECORATION);
+                if (helper.isVanilla()) {
 
-            if (helper.check(helper.checkKeys(Biomes.DARK_FOREST, Biomes.DARK_FOREST_HILLS), HabitatConfig.COMMON.fairyRingWhitelist, HabitatConfig.COMMON.fairyRingBlacklist))
-                helper.addStructure(HabitatConfiguredStructures.FAIRY_RING);
+                }
 
-            if (helper.checkKeys(Biomes.FLOWER_FOREST, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE))
-                helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 20, 3, 4);
-            else if (helper.checkCategories(Biome.BiomeCategory.FOREST, Biome.BiomeCategory.TAIGA, Biome.BiomeCategory.SAVANNA, Biome.BiomeCategory.JUNGLE))
-                helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 5, 3, 4);
+
+
+                //OLD
+                if (helper.checkCategory(Biome.BiomeCategory.JUNGLE) && !helper.checkKeys(Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS))
+                    helper.addFeature(HabitatConfiguredFeatures.PATCH_RAFFLESIA, GenerationStep.Decoration.VEGETAL_DECORATION);
+
+                if (helper.checkCategory(Biome.BiomeCategory.PLAINS))
+                    helper.addFeature(HabitatConfiguredFeatures.PATCH_KABLOOM_BUSH, GenerationStep.Decoration.VEGETAL_DECORATION);
+
+                if (helper.checkCategory(Biome.BiomeCategory.DESERT) || helper.checkCategory(Biome.BiomeCategory.MESA))
+                    helper.addFeature(HabitatConfiguredFeatures.PATCH_BALL_CACTUS, GenerationStep.Decoration.VEGETAL_DECORATION);
+
+                if (helper.checkKeys(Biomes.DARK_FOREST, Biomes.DARK_FOREST_HILLS))
+                    helper.addStructure(HabitatConfiguredStructures.FAIRY_RING);
+
+                if (helper.checkKeys(Biomes.FLOWER_FOREST, Biomes.JUNGLE, Biomes.JUNGLE_EDGE, Biomes.JUNGLE_HILLS, Biomes.BAMBOO_JUNGLE, Biomes.BAMBOO_JUNGLE_HILLS, Biomes.MODIFIED_JUNGLE, Biomes.MODIFIED_JUNGLE_EDGE))
+                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 20, 3, 4);
+                else if (helper.checkCategories(Biome.BiomeCategory.FOREST, Biome.BiomeCategory.TAIGA, Biome.BiomeCategory.SAVANNA, Biome.BiomeCategory.JUNGLE))
+                    helper.addCreatureSpawn(HabitatEntityTypes.PASSERINE.get(), 5, 3, 4);
+            }
         }
     }
 
@@ -69,15 +74,8 @@ public class HabitatBiomeLoadingEvent {
             spawns = event.getSpawns();
         }
 
-        private boolean check(boolean condition, ForgeConfigSpec.ConfigValue<String> whitelistConfig, ForgeConfigSpec.ConfigValue<String> blacklistConfig) {
-            String biome = event.getName().toString();
-            List<String> whitelist = Arrays.asList(StringUtils.deleteWhitespace(whitelistConfig.get()).split(","));
-            List<String> blacklist = Arrays.asList(StringUtils.deleteWhitespace(blacklistConfig.get()).split(","));
-            return condition && !blacklist.contains(biome) || whitelist.contains(biome);
-        }
-
-        private boolean check(ForgeConfigSpec.ConfigValue<String> whitelistConfig, ForgeConfigSpec.ConfigValue<String> blacklistConfig) {
-            return check(true, whitelistConfig, blacklistConfig);
+        private boolean isVanilla() {
+            return ResourceLocation.DEFAULT_NAMESPACE.equals(event.getName().getNamespace());
         }
 
         private boolean checkCategory(Biome.BiomeCategory category) {
