171ac429105e3ba18bec61e21a6a4fe5c90cbb59
SnappyDragon18 <pg61151@gmail.com>
2021-12-05 15:30:37 +0530

    Lowered max health to 4

--- src/main/java/mod/schnappdragon/habitat/common/entity/animal/Passerine.java
+++ src/main/java/mod/schnappdragon/habitat/common/entity/animal/Passerine.java
@@ -96,7 +96,7 @@ public class Passerine extends Animal implements FlyingAnimal {
 
     public static AttributeSupplier.Builder registerAttributes() {
         return Mob.createMobAttributes()
-                .add(Attributes.MAX_HEALTH, 5.0D)
+                .add(Attributes.MAX_HEALTH, 4.0D)
                 .add(Attributes.FLYING_SPEED, 0.8F)
                 .add(Attributes.MOVEMENT_SPEED, 0.2F);
     }
