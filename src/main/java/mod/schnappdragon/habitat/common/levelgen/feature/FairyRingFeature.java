d1e4f08c845cec91b19525bb94d9b46cd5092683
SnappyDragon18 <pg61151@gmail.com>
2023-03-13 20:49:21 +0530

    Fairy Ring configuration + radius increase

--- src/main/java/mod/schnappdragon/habitat/common/levelgen/feature/FairyRingFeature.java
+++ src/main/java/mod/schnappdragon/habitat/common/levelgen/feature/FairyRingFeature.java
@@ -1,40 +1,38 @@
 package mod.schnappdragon.habitat.common.levelgen.feature;
 
 import com.mojang.serialization.Codec;
-import mod.schnappdragon.habitat.common.block.FairyRingMushroomBlock;
-import mod.schnappdragon.habitat.core.Habitat;
-import mod.schnappdragon.habitat.core.registry.HabitatBlocks;
+import mod.schnappdragon.habitat.common.levelgen.feature.configuration.FairyRingFeatureConfiguration;
 import net.minecraft.core.BlockPos;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.tags.BlockTags;
 import net.minecraft.util.RandomSource;
 import net.minecraft.world.level.WorldGenLevel;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.level.chunk.ChunkGenerator;
-import net.minecraft.world.level.levelgen.feature.ConfiguredFeature;
 import net.minecraft.world.level.levelgen.feature.Feature;
 import net.minecraft.world.level.levelgen.feature.FeaturePlaceContext;
-import net.minecraft.world.level.levelgen.feature.configurations.NoneFeatureConfiguration;
+import net.minecraft.world.level.levelgen.placement.PlacedFeature;
 
-public class FairyRingFeature extends Feature<NoneFeatureConfiguration> {
-    public FairyRingFeature(Codec<NoneFeatureConfiguration> codec) {
+public class FairyRingFeature extends Feature<FairyRingFeatureConfiguration> {
+    public FairyRingFeature(Codec<FairyRingFeatureConfiguration> codec) {
         super(codec);
     }
 
-    public boolean place(FeaturePlaceContext<NoneFeatureConfiguration> context) {
+    public boolean place(FeaturePlaceContext<FairyRingFeatureConfiguration> context) {
         ChunkGenerator generator = context.chunkGenerator();
         WorldGenLevel world = context.level();
         BlockPos pos = context.origin();
         RandomSource rand = context.random();
-        boolean[] flag = {false};
+        FairyRingFeatureConfiguration config = context.config();
+        boolean[] flag = { false };
+        int rad = config.radius();
+        int x = 0;
+        int z = rad;
+        int p = 1 - rad;
 
-        int rad = 5, x = 0, z = rad, p = 1 - rad;
-
-        this.setMushroom(world, pos.offset(rad, 0, 0), rand, generator, flag);
-        this.setMushroom(world, pos.offset(-rad, 0, 0), rand, generator, flag);
-        this.setMushroom(world, pos.offset(0, 0, rad), rand, generator, flag);
-        this.setMushroom(world, pos.offset(0, 0, -rad), rand, generator, flag);
+        this.setMushroom(world, pos.offset(rad, 0, 0), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(-rad, 0, 0), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(0, 0, rad), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(0, 0, -rad), config, rand, generator, flag);
 
         while (x < z - 1) {
             x++;
@@ -45,41 +43,37 @@ public class FairyRingFeature extends Feature<NoneFeatureConfiguration> {
             } else
                 p = p + 2 * x + 1;
 
-            this.setMushrooms(world, pos, x, z, rand, generator, flag);
+            this.setMushrooms(world, pos, x, z, config, rand, generator, flag);
         }
 
         return true;
     }
 
-    public void setMushrooms(WorldGenLevel world, BlockPos pos, int x, int z, RandomSource rand, ChunkGenerator generator, boolean[] flag) {
-        this.setMushroom(world, pos.offset(-x, 0 , z), rand, generator, flag);
-        this.setMushroom(world, pos.offset(-z, 0 , x), rand, generator, flag);
-        this.setMushroom(world, pos.offset(-z, 0, -x), rand, generator, flag);
-        this.setMushroom(world, pos.offset(-x, 0 , -z), rand, generator, flag);
-        this.setMushroom(world, pos.offset(x, 0 , -z), rand, generator, flag);
-        this.setMushroom(world, pos.offset(z, 0 , -x), rand, generator, flag);
-        this.setMushroom(world, pos.offset(z, 0 , x), rand, generator, flag);
-        this.setMushroom(world, pos.offset(x, 0 , z), rand, generator, flag);
+    public void setMushrooms(WorldGenLevel world, BlockPos pos, int x, int z, FairyRingFeatureConfiguration config, RandomSource rand, ChunkGenerator generator, boolean[] flag) {
+        this.setMushroom(world, pos.offset(-x, 0 , z), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(-z, 0 , x), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(-z, 0, -x), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(-x, 0 , -z), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(x, 0 , -z), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(z, 0 , -x), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(z, 0 , x), config, rand, generator, flag);
+        this.setMushroom(world, pos.offset(x, 0 , z), config, rand, generator, flag);
     }
 
-    public void setMushroom(WorldGenLevel world, BlockPos pos, RandomSource rand, ChunkGenerator generator, boolean[] flag) {
+    public void setMushroom(WorldGenLevel world, BlockPos pos, FairyRingFeatureConfiguration config, RandomSource rand, ChunkGenerator generator, boolean[] flag) {
         pos = pos.below();
         BlockState base = world.getBlockState(pos.below());
 
         if (world.isEmptyBlock(pos) && base.is(BlockTags.DIRT)) {
             if (!flag[0] && rand.nextInt(8) == 0) {
-                ConfiguredFeature<?, ?> configuredfeature = world.registryAccess().registryOrThrow(Registry.CONFIGURED_FEATURE_REGISTRY).get(new ResourceLocation(Habitat.MODID, "huge_fairy_ring_mushroom"));
+                PlacedFeature placedFeature = config.feature().get();
 
-                if (configuredfeature.place(world, generator, rand, pos)) {
+                if (placedFeature.place(world, generator, rand, pos)) {
                     flag[0] = true;
                 } else
-                    this.setBlock(world, pos, this.getMushroom(rand));
+                    this.setBlock(world, pos, config.mushroomProvider().getState(rand, pos));
             } else
-                this.setBlock(world, pos, this.getMushroom(rand));
+                this.setBlock(world, pos, config.mushroomProvider().getState(rand, pos));
         }
-    }
-
-    private BlockState getMushroom(RandomSource random) {
-        return HabitatBlocks.FAIRY_RING_MUSHROOM.get().defaultBlockState().setValue(FairyRingMushroomBlock.MUSHROOMS, 1 + random.nextInt(4));
     }
 }
--- src/main/java/mod/schnappdragon/habitat/core/registry/HabitatFeatures.java
+++ src/main/java/mod/schnappdragon/habitat/core/registry/HabitatFeatures.java
@@ -4,6 +4,7 @@ import mod.schnappdragon.habitat.common.levelgen.feature.FairyRingFeature;
 import mod.schnappdragon.habitat.common.levelgen.feature.HugeBallCactusFeature;
 import mod.schnappdragon.habitat.common.levelgen.feature.HugeFairyRingMushroomFeature;
 import mod.schnappdragon.habitat.common.levelgen.feature.SlimeFernFeature;
+import mod.schnappdragon.habitat.common.levelgen.feature.configuration.FairyRingFeatureConfiguration;
 import mod.schnappdragon.habitat.common.levelgen.feature.configuration.HugeBallCactusFeatureConfiguration;
 import mod.schnappdragon.habitat.common.levelgen.feature.configuration.SlimeFernFeatureConfiguration;
 import mod.schnappdragon.habitat.core.Habitat;
@@ -20,8 +21,8 @@ public class HabitatFeatures {
     public final static RegistryObject<Feature<SlimeFernFeatureConfiguration>> SLIME_FERN_FEATURE = FEATURES.register("slime_fern",
             () -> new SlimeFernFeature(SlimeFernFeatureConfiguration.CODEC));
 
-    public final static RegistryObject<Feature<NoneFeatureConfiguration>> FAIRY_RING_FEATURE = FEATURES.register("fairy_ring",
-            () -> new FairyRingFeature(NoneFeatureConfiguration.CODEC));
+    public final static RegistryObject<Feature<FairyRingFeatureConfiguration>> FAIRY_RING_FEATURE = FEATURES.register("fairy_ring",
+            () -> new FairyRingFeature(FairyRingFeatureConfiguration.CODEC));
 
     public final static RegistryObject<Feature<HugeMushroomFeatureConfiguration>> HUGE_FAIRY_RING_MUSHROOM_FEATURE = FEATURES.register("huge_fairy_ring_mushroom",
             () -> new HugeFairyRingMushroomFeature(HugeMushroomFeatureConfiguration.CODEC));
