3e0dbe46999efa8f9243d802d289ae708c4e618b
SnappyDragon18 <pg61151@gmail.com>
2023-03-17 18:14:12 +0530

    Removed commented tags

--- src/main/java/mod/schnappdragon/habitat/core/tags/HabitatBlockTags.java
+++ src/main/java/mod/schnappdragon/habitat/core/tags/HabitatBlockTags.java
@@ -8,18 +8,11 @@ import net.minecraft.world.level.block.Block;
 
 public class HabitatBlockTags {
     public static final TagKey<Block> RAFFLESIA_PLACEABLE_ON = makeTag("rafflesia_placeable_on");
-    // public static final TagKey<Block> BALL_CACTUS_FLOWERS = makeTag("ball_cactus_flowers");
-    // public static final TagKey<Block> GROWING_BALL_CACTI = makeTag("growing_ball_cacti");
-    // public static final TagKey<Block> BALL_CACTI = makeTag("ball_cacti");
-    // public static final TagKey<Block> FLOWERING_BALL_CACTI = makeTag("flowering_ball_cacti");
     public static final TagKey<Block> BALL_CACTUS_GROWS_ON = makeTag("ball_cactus_grows_on");
     public static final TagKey<Block> BALL_CACTUS_FLOWER_PLACEABLE_ON = makeTag("ball_cactus_flower_placeable_on");
-    // public static final TagKey<Block> FAIRY_RING_MUSHROOM_STEMS = makeTag("fairy_ring_mushroom_stems");
     public static final TagKey<Block> PASSERINES_PERCHABLE_ON = makeTag("passerines_perchable_on");
     public static final TagKey<Block> PASSERINES_SPAWNABLE_ON = makeTag("passerines_spawnable_on");
     public static final TagKey<Block> EDELWEISS_SHRUB_PLACEABLE_ON = makeTag("edelweiss_shrub_placeable_on");
-    // public static final TagKey<Block> BALL_CACTUS_BLOCKS = makeTag("ball_cactus_blocks");
-    // public static final TagKey<Block> FLOWERING_BALL_CACTUS_BLOCKS = makeTag("flowering_ball_cactus_blocks");
     public static final TagKey<Block> POOKA_SPAWNABLE_ON = makeTag("pooka_spawnable_on");
 
     private static TagKey<Block> makeTag(String id) {
