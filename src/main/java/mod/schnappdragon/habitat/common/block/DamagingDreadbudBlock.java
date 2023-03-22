d30cb5134c5318aefe63f7dcaa4c716ce90fae37
SnappyDragon18 <pg61151@gmail.com>
2023-03-23 00:18:49 +0530

    Dreadbud damage

--- src/main/java/mod/schnappdragon/habitat/common/block/DamagingDreadbudBlock.java
+++ src/main/java/mod/schnappdragon/habitat/common/block/DamagingDreadbudBlock.java
@@ -1,15 +1,36 @@
 package mod.schnappdragon.habitat.common.block;
 
+import mod.schnappdragon.habitat.core.misc.HabitatDamageSources;
+import net.minecraft.core.BlockPos;
+import net.minecraft.world.entity.Entity;
+import net.minecraft.world.entity.LivingEntity;
+import net.minecraft.world.entity.Mob;
+import net.minecraft.world.level.BlockGetter;
+import net.minecraft.world.level.Level;
 import net.minecraft.world.level.block.Block;
+import net.minecraft.world.level.block.state.BlockState;
+import net.minecraft.world.level.pathfinder.BlockPathTypes;
 
 import javax.annotation.Nullable;
 import java.util.function.Supplier;
 
 public class DamagingDreadbudBlock extends DreadbudBlock {
-    private final int damage;
+    private final float damage;
 
-    public DamagingDreadbudBlock(int damage, @Nullable Supplier<Block> nextStage, float threshold, Properties properties) {
+    public DamagingDreadbudBlock(float damage, @Nullable Supplier<Block> nextStage, float threshold, Properties properties) {
         super(nextStage, threshold, properties);
         this.damage = damage;
+    }
+
+    public void entityInside(BlockState state, Level world, BlockPos pos, Entity entity) {
+        if (!world.isClientSide && entity instanceof LivingEntity && (entity.xOld != entity.getX() || entity.zOld != entity.getZ()) && (Math.abs(entity.getX() - entity.xOld) >= 0.003D || Math.abs(entity.getZ() - entity.zOld) >= 0.003D)) {
+            entity.hurt(HabitatDamageSources.DREADBUD, this.damage);
+        }
+    }
+
+    @Nullable
+    @Override
+    public BlockPathTypes getBlockPathType(BlockState state, BlockGetter world, BlockPos pos, @Nullable Mob entity) {
+        return BlockPathTypes.DAMAGE_OTHER;
     }
 }
--- src/main/java/mod/schnappdragon/habitat/core/misc/HabitatDamageSources.java
+++ src/main/java/mod/schnappdragon/habitat/core/misc/HabitatDamageSources.java
@@ -8,6 +8,8 @@ import net.minecraft.world.entity.Entity;
 import javax.annotation.Nullable;
 
 public class HabitatDamageSources {
+    public static final DamageSource DREADBUD = new DamageSource("habitat.dreadbud");
+
     public static DamageSource causeKabloomDamage(ThrownKabloomFruit kabloom, @Nullable Entity indirectEntityIn, boolean isExplosion) {
         IndirectEntityDamageSource source = new IndirectEntityDamageSource("habitat.kabloom", kabloom, indirectEntityIn);
         if (isExplosion) source.setExplosion();
