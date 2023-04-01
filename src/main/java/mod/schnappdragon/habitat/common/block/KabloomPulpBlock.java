12f8fa41ae0c713a06ff15f141a34498e8628f77
SnappyDragon18 <pg61151@gmail.com>
2023-04-01 17:49:08 +0530

    Kabloom Pulp Block rework again

--- src/main/java/mod/schnappdragon/habitat/common/block/KabloomPulpBlock.java
+++ src/main/java/mod/schnappdragon/habitat/common/block/KabloomPulpBlock.java
@@ -3,34 +3,27 @@ package mod.schnappdragon.habitat.common.block;
 import mod.schnappdragon.habitat.core.registry.HabitatEffects;
 import mod.schnappdragon.habitat.core.tags.HabitatBlockTags;
 import net.minecraft.core.BlockPos;
-import net.minecraft.sounds.SoundEvent;
 import net.minecraft.world.effect.MobEffectInstance;
 import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
 import net.minecraft.world.level.BlockGetter;
 import net.minecraft.world.level.Level;
+import net.minecraft.world.level.block.Block;
 import net.minecraft.world.level.block.HalfTransparentBlock;
 import net.minecraft.world.level.block.state.BlockState;
 import net.minecraft.world.phys.Vec3;
 import net.minecraft.world.phys.shapes.CollisionContext;
-import net.minecraft.world.phys.shapes.Shapes;
 import net.minecraft.world.phys.shapes.VoxelShape;
 
 public class KabloomPulpBlock extends HalfTransparentBlock {
+    protected static final VoxelShape SHAPE = Block.box(0.0D, 0.0D, 0.0D, 16.0D, 15.0D, 16.0D);
+
     public KabloomPulpBlock(Properties properties) {
         super(properties);
     }
 
     public VoxelShape getCollisionShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return Shapes.empty();
-    }
-
-    public VoxelShape getVisualShape(BlockState state, BlockGetter world, BlockPos pos, CollisionContext context) {
-        return Shapes.empty();
-    }
-
-    public VoxelShape getOcclusionShape(BlockState state, BlockGetter world, BlockPos pos) {
-        return Shapes.empty();
+        return SHAPE;
     }
 
     @Override
@@ -48,20 +41,13 @@ public class KabloomPulpBlock extends HalfTransparentBlock {
     }
 
     @Override
-    public void entityInside(BlockState state, Level world, BlockPos pos, Entity entity) {
-        entity.makeStuckInBlock(state, new Vec3(0.25F, 0.2F, 0.25F));
+    public void stepOn(Level world, BlockPos pos, BlockState state, Entity entity) {
+        entity.makeStuckInBlock(state, new Vec3(0.85F, 0.8F, 0.85F));
 
         if (entity instanceof LivingEntity living) {
             living.addEffect(new MobEffectInstance(HabitatEffects.BLAST_ENDURANCE.get(), 100));
         }
-    }
 
-    @Override
-    public void fallOn(Level world, BlockState state, BlockPos pos, Entity entity, float distance) {
-        if (distance >= 4.0F && entity instanceof LivingEntity livingentity) {
-            LivingEntity.Fallsounds fallSounds = livingentity.getFallSounds();
-            SoundEvent soundevent = distance < 7.0F ? fallSounds.small() : fallSounds.big();
-            entity.playSound(soundevent, 1.0F, 1.0F);
-        }
+        super.stepOn(world, pos, state, entity);
     }
 }
