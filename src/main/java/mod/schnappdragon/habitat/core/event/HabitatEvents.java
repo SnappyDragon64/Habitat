7a9c691d777b928740112318b9326543bd36cc3a
SnappyDragon64 <pg61151@gmail.com>
2023-07-22 18:09:55 +0530

    Fixes + rebalanced Prickling
    level 1: 25% chance for 1-2 damage
    level 2: 50% chance for 1-4 damage

--- src/main/java/mod/schnappdragon/habitat/core/event/HabitatEvents.java
+++ src/main/java/mod/schnappdragon/habitat/core/event/HabitatEvents.java
@@ -1,43 +1,23 @@
 package mod.schnappdragon.habitat.core.event;
 
 import mod.schnappdragon.habitat.core.Habitat;
-import mod.schnappdragon.habitat.core.registry.HabitatBlocks;
 import mod.schnappdragon.habitat.core.registry.HabitatEffects;
-import net.minecraft.core.Registry;
-import net.minecraft.resources.ResourceLocation;
 import net.minecraft.server.level.ServerPlayer;
 import net.minecraft.stats.Stats;
+import net.minecraft.tags.DamageTypeTags;
 import net.minecraft.util.Mth;
 import net.minecraft.world.damagesource.DamageSource;
 import net.minecraft.world.effect.MobEffectInstance;
+import net.minecraft.world.entity.Entity;
 import net.minecraft.world.entity.LivingEntity;
-import net.minecraft.world.entity.ai.village.poi.PoiType;
-import net.minecraft.world.level.block.state.BlockState;
 import net.minecraftforge.event.entity.living.LivingDamageEvent;
 import net.minecraftforge.event.entity.living.LivingHurtEvent;
 import net.minecraftforge.event.entity.living.MobEffectEvent;
 import net.minecraftforge.eventbus.api.SubscribeEvent;
 import net.minecraftforge.fml.common.Mod;
-import net.minecraftforge.registries.ForgeRegistries;
-import net.minecraftforge.registries.RegisterEvent;
-
-import java.util.HashSet;
 
 @Mod.EventBusSubscriber(modid = Habitat.MODID)
 public class HabitatEvents {
-
-    /*
-     * Used to registry replace Beehive PoiType
-     */
-
-    @SubscribeEvent
-    public static void addToBeehivePoiType(RegisterEvent event) {
-        ResourceLocation beePOIRL = new ResourceLocation("minecraft:bee_nest");
-        PoiType beePOI = ForgeRegistries.POI_TYPES.getValue(beePOIRL);
-        HashSet<BlockState> newSet = new HashSet<>(beePOI.matchingStates());
-        newSet.addAll(HabitatBlocks.FAIRY_RING_MUSHROOM_BEEHIVE.get().getStateDefinition().getPossibleStates());
-        event.register(Registry.POINT_OF_INTEREST_TYPE_REGISTRY, beePOIRL, () -> new PoiType(newSet, beePOI.validRange(),  beePOI.maxTickets()));
-    }
 
     /*
      * Used to reduce explosion damage if LivingEntity has Blast Endurance effect.
@@ -45,7 +25,7 @@ public class HabitatEvents {
 
     @SubscribeEvent
     public static void reduceExplosionDamage(LivingDamageEvent event) {
-        if (event.getEntity().hasEffect(HabitatEffects.BLAST_ENDURANCE.get()) && event.getSource().isExplosion()) {
+        if (event.getEntity().hasEffect(HabitatEffects.BLAST_ENDURANCE.get()) && event.getSource().is(DamageTypeTags.IS_EXPLOSION)) {
             LivingEntity livingEntity = event.getEntity();
             DamageSource source = event.getSource();
 
@@ -72,10 +52,11 @@ public class HabitatEvents {
         if (event.getEntity().hasEffect(HabitatEffects.PRICKLING.get())) {
             LivingEntity livingEntity = event.getEntity();
             DamageSource source = event.getSource();
+            Entity directEntity = source.getDirectEntity();
             int lvl = livingEntity.getEffect(HabitatEffects.PRICKLING.get()).getAmplifier();
 
-            if (livingEntity.getRandom().nextInt(4) < 2 + lvl && !source.isMagic() && !source.isExplosion() && source.getDirectEntity() instanceof LivingEntity attacker) {
-                attacker.hurt(DamageSource.thorns(livingEntity), 1.0F + lvl + livingEntity.getRandom().nextInt(2 + lvl));
+            if (directEntity != null && livingEntity.getRandom().nextInt(4) < 1 + lvl) {
+                directEntity.hurt(livingEntity.damageSources().thorns(livingEntity), 1.0F + livingEntity.getRandom().nextInt(1 + 2 * lvl));
             }
         }
     }
