854f5fce3479e7894b2b3251a01ae05a00493f73
SnappyDragon64 <pg61151@gmail.com>
2023-11-16 20:12:07 +0530

    Added dreadbuds to creative inventory

--- src/main/java/mod/schnappdragon/habitat/core/registry/HabitatItems.java
+++ src/main/java/mod/schnappdragon/habitat/core/registry/HabitatItems.java
@@ -114,11 +114,11 @@ public class HabitatItems {
     public static final RegistryObject<Item> TALL_WHITE_ANTHURIUM = registerBlockItem("tall_white_anthurium", HabitatBlocks.TALL_WHITE_ANTHURIUM);
     public static final RegistryObject<Item> TALL_YELLOW_ANTHURIUM = registerBlockItem("tall_yellow_anthurium", HabitatBlocks.TALL_YELLOW_ANTHURIUM);
 
-    public static final RegistryObject<Item> BLOWBALL = ITEMS.register("blowball",
-            () -> new BlowballItem(HabitatBlocks.BLOWBALL.get(), getProperties()));
-
     public static final RegistryObject<Item> DREADBUD = ITEMS.register("dreadbud",
             () -> new ItemNameBlockItem(HabitatBlocks.BLOOMING_DREADBUD.get(), new Item.Properties()));
+
+    public static final RegistryObject<Item> BLOWBALL = ITEMS.register("blowball",
+            () -> new BlowballItem(HabitatBlocks.BLOWBALL.get(), getProperties()));
 
     @SubscribeEvent
     public static void registerCreativeTabsItem(BuildCreativeModeTabContentsEvent event) {
@@ -157,6 +157,7 @@ public class HabitatItems {
             event.accept(TALL_RED_ANTHURIUM);
             event.accept(TALL_WHITE_ANTHURIUM);
             event.accept(TALL_YELLOW_ANTHURIUM);
+            event.accept(DREADBUD);
             event.accept(BLOWBALL);
         } else if (event.getTabKey() == CreativeModeTabs.FUNCTIONAL_BLOCKS) {
             event.accept(FAIRY_RING_MUSHROOM);
