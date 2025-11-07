087094ba37a529503eba93d069dcab37eadb8f5b
SnappyDragon64 <pg61151@gmail.com>
2025-11-08 03:16:26 +0530

    Leftover woodset cleanup

--- src/main/java/mod/schnappdragon/habitat/common/block/state/properties/HabitatBlockStateProperties.java
+++ src/main/java/mod/schnappdragon/habitat/common/block/state/properties/HabitatBlockStateProperties.java
@@ -9,10 +9,4 @@ public class HabitatBlockStateProperties {
     public static final BooleanProperty HAS_STEW = BooleanProperty.create("has_stew");
     public static final BooleanProperty SLIMY = BooleanProperty.create("slimy");
     public static final IntegerProperty MUSHROOMS_1_4 = IntegerProperty.create("mushrooms", 1, 4);
-    public static final BooleanProperty CHAINED_UP = BooleanProperty.create("chained_up");
-    public static final BooleanProperty CHAINED_DOWN = BooleanProperty.create("chained_down");
-    public static final BooleanProperty CHAINED_NORTH = BooleanProperty.create("chained_north");
-    public static final BooleanProperty CHAINED_EAST = BooleanProperty.create("chained_east");
-    public static final BooleanProperty CHAINED_SOUTH = BooleanProperty.create("chained_south");
-    public static final BooleanProperty CHAINED_WEST = BooleanProperty.create("chained_west");
 }
