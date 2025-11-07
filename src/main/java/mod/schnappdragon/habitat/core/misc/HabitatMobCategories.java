4233faa586aca6c0e9c32b8e9922df6eaaa408e1
SnappyDragon64 <pg61151@gmail.com>
2025-11-08 00:26:05 +0530

    Tuned passerine spawning

--- src/main/java/mod/schnappdragon/habitat/core/misc/HabitatMobCategories.java
+++ src/main/java/mod/schnappdragon/habitat/core/misc/HabitatMobCategories.java
@@ -4,7 +4,7 @@ import mod.schnappdragon.habitat.core.Habitat;
 import net.minecraft.world.entity.MobCategory;
 
 public class HabitatMobCategories {
-    public static final MobCategory PASSERINE = create("passerine", 15, true, false, 64);
+    public static final MobCategory PASSERINE = create("passerine", 10, true, false, 64);
 
     private static MobCategory create(String name, int maxCap, boolean isFriendly, boolean isPersistent, int despawnDistance) {
         String namespacedId = Habitat.MODID + "_" + name;
